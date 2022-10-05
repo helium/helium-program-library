49ea49b7ee7c26dab799073f37a909e1d1109ad5
ThornM9 <tm.jalal@gmail.com>
2022-10-05 13:08:26 +1000

    import

--- packages/data-credits-sdk/src/index.ts
+++ packages/data-credits-sdk/src/index.ts
@@ -6,6 +6,7 @@ import {
 } from "@project-serum/anchor";
 import { PROGRAM_ID } from "./constants";
 import { ataResolver, combineResolvers } from "@helium-foundation/spl-utils";
+import { dataCreditsKey } from "./pdas";
 
 
 export async function init(provider: AnchorProvider, dataCreditsProgramId: PublicKey = PROGRAM_ID, dataCreditsIdl?: any): Promise<Program<DataCredits>> {
