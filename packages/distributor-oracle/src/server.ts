cbe456a41b433abdbe5bda3d4a9290b9d9ef3485
Bryan Zettler <bryanzettler@gmail.com>
2022-10-20 16:49:12 -0500

    added dot env to oracale

--- packages/distributor-oracle/src/server.ts
+++ packages/distributor-oracle/src/server.ts
@@ -1,13 +1,26 @@
-import express, {Application, Request, Response} from 'express';
-import { PublicKey, Transaction, Keypair, TransactionInstruction } from '@solana/web3.js'
-import bodyParser from 'body-parser'
-import { init, PROGRAM_ID } from '../../lazy-distributor-sdk/src';
-import { AnchorProvider, BorshInstructionCoder, Program, setProvider, getProvider } from '@project-serum/anchor';
-import { LazyDistributor } from '../../../target/types/lazy_distributor';
-import fs from 'fs';
+import dotenv from "dotenv";
+dotenv.config();
+import express, { Application, Request, Response } from "express";
+import {
+  PublicKey,
+  Transaction,
+  Keypair,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import bodyParser from "body-parser";
+import { init, PROGRAM_ID } from "../../lazy-distributor-sdk/src";
+import {
+  AnchorProvider,
+  BorshInstructionCoder,
+  Program,
+  setProvider,
+  getProvider,
+} from "@project-serum/anchor";
+import { LazyDistributor } from "../../../target/types/lazy_distributor";
+import fs from "fs";
 
 export interface Database {
-  getCurrentRewards: (mintKey: PublicKey) => Promise<number>
+  getCurrentRewards: (mintKey: PublicKey) => Promise<number>;
 }
 
 export class DatabaseMock implements Database {
@@ -16,16 +29,15 @@ export class DatabaseMock implements Database {
   }
 }
 
-
 export class OracleServer {
   app: Application;
   port = 8080;
   private server: any;
 
   constructor(
-    public program: Program<LazyDistributor>, 
+    public program: Program<LazyDistributor>,
     private oracle: Keypair,
-    public db: Database,
+    public db: Database
   ) {
     const app = express();
     app.use(bodyParser.json());
@@ -40,7 +52,7 @@ export class OracleServer {
   }
 
   public close() {
-    this.server.close()
+    this.server.close();
   }
 
   private addRoutes() {
@@ -51,19 +63,19 @@ export class OracleServer {
   private async getCurrentRewardsHandler(req: Request, res: Response) {
     const mintStr = req.query.mint;
     if (!mintStr) {
-      res.status(400).json({error: "No mint key provided"});
+      res.status(400).json({ error: "No mint key provided" });
       return;
     }
     let mint: PublicKey;
     try {
       mint = new PublicKey(mintStr);
-    } catch(err) {
-      res.status(400).json({error: "Invalid mint key"});
+    } catch (err) {
+      res.status(400).json({ error: "Invalid mint key" });
       return;
     }
 
     const currentRewards = await this.db.getCurrentRewards(mint);
-  
+
     res.json({
       currentRewards,
     });
@@ -71,42 +83,58 @@ export class OracleServer {
 
   private async signTransactionHandler(req: Request, res: Response) {
     if (!req.body.transaction) {
-      res.status(400).json({error: "No transaction field"});
+      res.status(400).json({ error: "No transaction field" });
       return;
     }
-  
+
     const tx = Transaction.from(req.body.transaction.data);
-  
+
     // validate only interacts with LD program and only calls setCurrentRewards and distributeRewards
     const setRewardIxs: TransactionInstruction[] = [];
     for (const ix of tx.instructions) {
       if (!ix.programId.equals(PROGRAM_ID)) {
-        res.status(400).json({error: "Invalid instructions in transaction"});
+        res.status(400).json({ error: "Invalid instructions in transaction" });
         return;
       }
-      let decoded = (this.program.coder.instruction as BorshInstructionCoder).decode(ix.data);
-      if (!decoded || (decoded.name !== "setCurrentRewardsV0" && decoded.name !== "distributeRewardsV0")) {
-        res.status(400).json({error: "Invalid instructions in transaction"});
-        return
+      let decoded = (
+        this.program.coder.instruction as BorshInstructionCoder
+      ).decode(ix.data);
+      if (
+        !decoded ||
+        (decoded.name !== "setCurrentRewardsV0" &&
+          decoded.name !== "distributeRewardsV0")
+      ) {
+        res.status(400).json({ error: "Invalid instructions in transaction" });
+        return;
       }
       if (decoded.name === "setCurrentRewardsV0") setRewardIxs.push(ix);
     }
 
     const setRewardsIx = this.program.idl.instructions.find(
-      (x) => x.name === "setCurrentRewardsV0")!
+      (x) => x.name === "setCurrentRewardsV0"
+    )!;
     const oracleKeyIdx = setRewardsIx.accounts.findIndex(
-        (x) => x.name === "oracle")!;
-    const recipientIdx = setRewardsIx.accounts.findIndex((x) => x.name === "recipient")!;
+      (x) => x.name === "oracle"
+    )!;
+    const recipientIdx = setRewardsIx.accounts.findIndex(
+      (x) => x.name === "recipient"
+    )!;
     // validate setRewards value for this oracle is correct
     for (const ix of setRewardIxs) {
       if (ix.keys[oracleKeyIdx].pubkey.equals(this.oracle.publicKey)) {
-        let decoded = (this.program.coder.instruction as BorshInstructionCoder).decode(ix.data);
-        const recipientAcc = await this.program.account.recipientV0.fetch(ix.keys[recipientIdx].pubkey);
+        let decoded = (
+          this.program.coder.instruction as BorshInstructionCoder
+        ).decode(ix.data);
+        const recipientAcc = await this.program.account.recipientV0.fetch(
+          ix.keys[recipientIdx].pubkey
+        );
 
-        const currentRewards = await this.db.getCurrentRewards(recipientAcc.mint);
+        const currentRewards = await this.db.getCurrentRewards(
+          recipientAcc.mint
+        );
         // @ts-ignore
         if (decoded.data.args.currentRewards.toNumber() > currentRewards) {
-          res.status(400).json({error: "Invalid amount"});
+          res.status(400).json({ error: "Invalid amount" });
           return;
         }
       }
@@ -114,29 +142,40 @@ export class OracleServer {
 
     // validate that this oracle is not the fee payer
     if (tx.feePayer?.equals(this.oracle.publicKey)) {
-      res.status(400).json({error: "Cannot set this oracle as the fee payer"});
+      res
+        .status(400)
+        .json({ error: "Cannot set this oracle as the fee payer" });
       return;
     }
 
     tx.partialSign(this.oracle);
-    
-    const serialized = tx.serialize({ requireAllSignatures: false, verifySignatures: false});
-    res.json({success: true, transaction: serialized})
+
+    const serialized = tx.serialize({
+      requireAllSignatures: false,
+      verifySignatures: false,
+    });
+    res.json({ success: true, transaction: serialized });
   }
 }
 
-(async function() {
+(async function () {
   if (process.argv.length > 2 && process.argv[2] == "serve") {
     // driver code for running server
     setProvider(AnchorProvider.env());
     const provider = getProvider() as AnchorProvider;
     const oracleKeypair = Keypair.fromSecretKey(
       new Uint8Array(
-        JSON.parse(fs.readFileSync((process.env.ORACLE_KEYPAIR_PATH || process.env.ANCHOR_WALLET)!).toString())
+        JSON.parse(
+          fs
+            .readFileSync(
+              (process.env.ORACLE_KEYPAIR_PATH || process.env.ANCHOR_WALLET)!
+            )
+            .toString()
+        )
       )
     );
     const program = await init(provider);
-    const server = new OracleServer(program, oracleKeypair, new DatabaseMock);
+    const server = new OracleServer(program, oracleKeypair, new DatabaseMock());
     server.start();
   }
-})()
+})();
