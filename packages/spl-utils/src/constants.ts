7ef0cd8dfd88b4a1c6e04b030e00ea256be1cc5a
Chewing Glass <chewinglass123@gmail.com>
2022-11-02 10:32:03 -0500

    Fix rust

--- packages/spl-utils/src/constants.ts
+++ packages/spl-utils/src/constants.ts
@@ -1,9 +1,9 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const DC_MINT = new PublicKey("6YLxsKozpj9s87rsr7Z4MBB8URBqZxq9K9xtqPKp43de");
+export const DC_MINT = new PublicKey("CnU1ySY7q5qgYowLACz23d515wwakiEt5ujx8k1f6E34");
 
-export const HNT_MINT = new PublicKey("7NaJ3tctzZ7c1SLCWL59m5Ku7E3qVH1KkF2DNiBtxCTk");
+export const HNT_MINT = new PublicKey("Fd1ceDiVgfRQw3j9HAn3aarrkqUqTgHRBbHStRp1u2ck");
 
 export const MOBILE_MINT = new PublicKey(
-  "5DDtiXxeDq6kTt6XtUXM8oZ7A2xnBed8SRSTQzhYS4Qs"
+  "783c5RUqWpoioXZujWmas97hVVom5BpMncsBcbnPVfyR"
 );
