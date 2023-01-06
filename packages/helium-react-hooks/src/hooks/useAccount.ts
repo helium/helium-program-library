83f15d43924ad97a84be2394a4f84fb5a6e7c7ce
Luis Perrone <perronef5@users.noreply.github.com>
2023-01-06 19:15:27 +0100

    Update useAccount to not infinite render on RN

--- packages/helium-react-hooks/src/hooks/useAccount.ts
+++ packages/helium-react-hooks/src/hooks/useAccount.ts
@@ -1,18 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { PublicKey, AccountInfo } from "@solana/web3.js";
 import { useAccountFetchCache } from "./useAccountFetchCache";
 import { TypedAccountParser } from "@helium/spl-utils";
+import { useAsync } from 'react-async-hook'
 
 export interface ParsedAccountBase {
-  pubkey: PublicKey;
-  account: AccountInfo<Buffer>;
-  info: any; // TODO: change to unkown
+  pubkey: PublicKey
+  account: AccountInfo<Buffer>
+  info: unknown
 }
 
 export interface UseAccountState<T> {
-  loading: boolean;
-  account?: AccountInfo<Buffer>;
-  info?: T;
+  loading: boolean
+  account?: AccountInfo<Buffer>
+  info?: T
 }
 
 /**
@@ -27,137 +28,145 @@ export interface UseAccountState<T> {
 export function useAccount<T>(
   key: null | undefined | PublicKey,
   parser?: TypedAccountParser<T>,
-  isStatic: Boolean = false // Set if the accounts data will never change, optimisation to lower websocket usage.
+  isStatic = false, // Set if the accounts data will never change, optimisation to lower websocket usage.
 ): UseAccountState<T> {
   const cache = useAccountFetchCache();
   const [state, setState] = useState<UseAccountState<T>>({
     loading: true,
-  });
+  })
 
   const parsedAccountBaseParser = (
     pubkey: PublicKey,
-    data: AccountInfo<Buffer>
+    data: AccountInfo<Buffer>,
   ): ParsedAccountBase => {
     try {
-      const info = parser!(pubkey, data);
-      return {
-        pubkey,
-        account: data,
-        info,
-      };
-    } catch (e: any) {
-      console.error("Error while parsing", e);
+      if (parser) {
+        const info = parser(pubkey, data)
+        return {
+          pubkey,
+          account: data,
+          info,
+        }
+      }
+
       return {
         pubkey,
         account: data,
         info: undefined,
-      };
+      }
+    } catch (e) {
+      console.error(`Error while parsing: ${(e as Error).message}`)
+      return {
+        pubkey,
+        account: data,
+        info: undefined,
+      }
     }
-  };
+  }
 
-  const id = typeof key === "string" ? key : key?.toBase58();
+  const id = typeof key === 'string' ? key : key?.toBase58()
 
-  useEffect(() => {
+  useAsync(async () => {
     // Occassionally, dispose can get called while the cache promise is still going.
     // In that case, we want to dispose immediately.
-    let shouldDisposeImmediately = false;
-
+    let shouldDisposeImmediately = false
     let disposeWatch = () => {
-      shouldDisposeImmediately = true;
-    };
-
-    if (!id || !cache) {
-      setState({ loading: false });
-      return;
-    } else {
-      setState({ loading: true });
+      shouldDisposeImmediately = true
     }
-
-    let prevInfo = state.info;
+    if (!id || !cache) {
+      setState((s) => (!s.loading ? s : { loading: false }))
+      return
+    }
+    setState((s) => (s.loading ? s : { loading: true }))
+    let prevInfo = state.info
     cache
       .searchAndWatch(
         id,
         parser ? parsedAccountBaseParser : undefined,
-        isStatic
+        isStatic,
       )
       .then(([acc, dispose]) => {
         if (shouldDisposeImmediately) {
-          dispose();
-          shouldDisposeImmediately = false;
+          dispose()
+          shouldDisposeImmediately = false
         }
-        disposeWatch = dispose;
+        disposeWatch = dispose
         if (acc) {
           try {
             const nextInfo = mergePublicKeys(
               prevInfo,
-              (parser && parser(acc.pubkey, acc!.account)) as any
-            );
-            prevInfo = nextInfo;
+              parser && parser(acc.pubkey, acc?.account),
+            )
+            prevInfo = nextInfo
             setState({
               loading: false,
               info: nextInfo,
               account: acc.account,
-            });
-          } catch (e: any) {
-            console.error("Error while parsing", e);
+            })
+          } catch (e) {
+            console.error(`Error while parsing: ${(e as Error).message}`)
             setState({
               loading: false,
               info: undefined,
               account: acc.account,
-            });
+            })
           }
         } else {
-          setState({ loading: false });
+          setState((s) => (!s.loading ? s : { loading: false }))
         }
       })
       .catch((e) => {
-        console.error(e);
-        setState({ loading: false });
-      });
-
+        console.error(e)
+        setState((s) => (!s.loading ? s : { loading: false }))
+      })
     const disposeEmitter = cache.emitter.onCache((e) => {
-      const event = e;
+      const event = e
       if (event.id === id) {
         cache
           .search(id, parser ? parsedAccountBaseParser : undefined)
           .then((acc) => {
-            if (acc && acc.account != state.account) {
+            if (acc && acc.account !== state.account) {
               try {
                 setState({
                   loading: false,
                   info: mergePublicKeys(
                     state.info,
-                    parser && (parser(acc.pubkey, acc!.account) as any)
+                    parser && parser(acc.pubkey, acc.account),
                   ),
-                  account: acc!.account,
-                });
-              } catch (e: any) {
-                console.error("Error while parsing", e);
+                  account: acc.account,
+                })
+              } catch (err) {
+                console.error(`Error while parsing: ${(err as Error).message}`)
                 setState({
                   loading: false,
                   info: undefined,
                   account: acc.account,
-                });
+                })
               }
             }
-          });
+          })
       }
-    });
+    })
     return () => {
-      disposeEmitter();
-      setTimeout(disposeWatch, 30 * 1000); // Keep cached accounts around for 30s in case a rerender is causing reuse
-    };
-  }, [cache, id, !parser]); // only trigger on change to parser if it wasn't defined before.
+      disposeEmitter()
+      setTimeout(disposeWatch, 30 * 1000) // Keep cached accounts around for 30s in case a rerender is causing reuse
+    }
+  }, []) // only trigger on change to parser if it wasn't defined before.
 
-  return state;
+  return state
 }
 
-function isPureObject(input: any) {
+/**
+ *
+ * @param input
+ * @returns
+ */
+function isPureObject(input: typeof Object | null) {
   return (
-    null !== input &&
-    typeof input === "object" &&
+    input !== null &&
+    typeof input === 'object' &&
     Object.getPrototypeOf(input).isPrototypeOf(Object)
-  );
+  )
 }
 
 /**
@@ -165,9 +174,10 @@ function isPureObject(input: any) {
  * actually the same, just a new JS object. This will cause a lot of useMemo
  * to fail.
  */
-function mergePublicKeys(arg0: any | undefined, arg1: any | undefined): any {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function mergePublicKeys(arg0: any, arg1: any) {
   if (!isPureObject(arg1) || !arg1 || !arg0) {
-    return arg1;
+    return arg1
   }
 
   return Object.entries(arg1).reduce((acc, [key, value]) => {
@@ -177,11 +187,12 @@ function mergePublicKeys(arg0: any | undefined, arg1: any | undefined): any {
       arg0[key] &&
       arg1[key].equals(arg0[key])
     ) {
-      acc[key] = arg0[key];
+      acc[key as keyof typeof acc] = arg0[key]
     } else {
-      acc[key] = value;
+      acc[key as keyof typeof acc] = value
     }
 
-    return acc;
-  }, {} as Record<string, any>);
+    return acc
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  }, {} as Record<string, any>)
 }
