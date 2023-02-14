f951942f7c31faa33078b83d93489746b074e42c
Bryan Zettler <bryanzettler@gmail.com>
2023-02-14 16:00:56 -0600

    tweak script

--- scripts/reset-vsr-voting-mint.sh
+++ scripts/reset-vsr-voting-mint.sh
@@ -6,15 +6,14 @@ if [ "$CLUSTER" == "mainnet" ]; then
     CLUSTER_URL='https://api.mainnet-beta.solana.com'    
 elif [ "$CLUSTER" == "devnet" ]; then
     CLUSTER_URL='https://api.devnet.solana.com'
-elsef
+else
     CLUSTER_URL='http://127.0.0.1:8899'
 fi
 
-npx ts-node --project ./packages/helium-cli/tsconfig.cjs.json ./packages/helium-cli/src/reset-clockwork.ts \
+npx ts-node --project ./packages/helium-cli/tsconfig.cjs.json ./packages/helium-cli/src/reset-vsr-voting-mint.ts \
     --dntMint $(solana address -k packages/helium-cli/keypairs/mobile.json) -u $CLUSTER_URL --resetDaoThread --resetSubDaoThread \
     --hntMint $(solana address -k packages/helium-cli/keypairs/hnt.json)
 
-npx ts-node --project ./packages/helium-cli/tsconfig.cjs.json ./packages/helium-cli/src/reset-clockwork.ts \
+npx ts-node --project ./packages/helium-cli/tsconfig.cjs.json ./packages/helium-cli/src/reset-vsr-voting-mint.ts \
     --dntMint $(solana address -k packages/helium-cli/keypairs/iot.json) -u $CLUSTER_URL --resetSubDaoThread \
-    --hntMint $(solana address -k packages/helium-cli/keypairs/hnt.json)
-
+    --hntMint $(solana address -k packages/helium-cli/keypairs/hnt.json)    
