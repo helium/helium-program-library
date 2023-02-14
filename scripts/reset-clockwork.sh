6b78b5d80a9fc6e7c0ab40fcebb0dddabe8a2ded
Bryan <bryanzettler@gmail.com>
2023-02-14 16:09:21 -0600

    Update scripts/reset-clockwork.sh

--- scripts/reset-clockwork.sh
+++ scripts/reset-clockwork.sh
@@ -6,7 +6,7 @@ if [ "$CLUSTER" == "mainnet" ]; then
     CLUSTER_URL='https://api.mainnet-beta.solana.com'    
 elif [ "$CLUSTER" == "devnet" ]; then
     CLUSTER_URL='https://api.devnet.solana.com'
-elsef
+else
     CLUSTER_URL='http://127.0.0.1:8899'
 fi
 
