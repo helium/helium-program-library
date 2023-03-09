3548920d4db6f20e7d0b349c0d46acd873792938
Bryan Zettler <bryanzettler@gmail.com>
2023-03-09 12:09:15 -0600

    creating maxVoterWeight now

--- programs/voter-stake-registry/src/instructions/update_max_voter_weight_v0.rs
+++ programs/voter-stake-registry/src/instructions/update_max_voter_weight_v0.rs
@@ -1,50 +1,50 @@
+use crate::state::*;
 use anchor_lang::prelude::*;
 use anchor_spl::token::Mint;
+use std::mem::size_of;
 
-use crate::state::{MaxVoterWeightRecord, Registrar};
-
-/// Creates MaxVoterWeightRecord used by spl-gov
-/// This instruction should only be executed once per realm/governing_token_mint to create the account
 #[derive(Accounts)]
 pub struct UpdateMaxVoterWeightV0<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
   #[account(
-        init_if_needed,
-        seeds = [ b"max-voter-weight-record".as_ref(),
-                registrar.realm.as_ref(),
-                registrar.realm_governing_token_mint.as_ref()],
-        bump,
-        payer = payer,
-        space = MaxVoterWeightRecord::get_space()
-    )]
-  pub max_voter_weight_record: Account<'info, MaxVoterWeightRecord>,
-  #[account(
-      has_one = realm_governing_token_mint
-    )]
+    has_one = realm_governing_token_mint
+  )]
   pub registrar: Box<Account<'info, Registrar>>,
   pub realm_governing_token_mint: Account<'info, Mint>,
 
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
+  #[account(
+    init_if_needed,
+    payer = payer,
+    space = 8 + size_of::<MaxVoterWeightRecord>(),
+    seeds = [b"max-voter-weight-record".as_ref(), registrar.realm.as_ref(), registrar.realm_governing_token_mint.as_ref()],
+    bump,
+  )]
+  pub max_voter_weight_record: Account<'info, MaxVoterWeightRecord>,
   pub system_program: Program<'info, System>,
 }
 
+/// Creates MaxVoterWeightRecord used by spl-gov
+/// This instruction should only be executed once per realm/governing_token_mint to create the account
 pub fn handler(ctx: Context<UpdateMaxVoterWeightV0>) -> Result<()> {
+  let registrar = &ctx.accounts.registrar;
   let max_voter_weight_record = &mut ctx.accounts.max_voter_weight_record;
 
-  let registrar = &ctx.accounts.registrar;
   max_voter_weight_record.realm = registrar.realm;
   max_voter_weight_record.governing_token_mint = registrar.realm_governing_token_mint;
-
   max_voter_weight_record.max_voter_weight_expiry = None;
+
   let config_idx =
     registrar.voting_mint_config_index(ctx.accounts.realm_governing_token_mint.key())?;
-  let max_scale = registrar.voting_mints[config_idx].max_extra_lockup_vote_weight_scaled_factor;
-  max_voter_weight_record.max_voter_weight = ctx
-    .accounts
-    .realm_governing_token_mint
-    .supply
-    .checked_mul(max_scale)
+
+  let governing_mint_supply = ctx.accounts.realm_governing_token_mint.supply;
+
+  let max_locked_vote_weight =
+    registrar.voting_mints[config_idx].max_extra_lockup_vote_weight(governing_mint_supply)?;
+  let genesis_multiplier = registrar.voting_mints[config_idx].genesis_vote_power_multiplier;
+
+  max_voter_weight_record.max_voter_weight = max_locked_vote_weight
+    .checked_mul(genesis_multiplier as u64)
     .unwrap();
 
   Ok(())
