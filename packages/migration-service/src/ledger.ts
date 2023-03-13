0d6c967e541b65bbbedef44f8b1d2a1e6ffcfa25
Chewing Glass <chewinglass123@gmail.com>
2023-03-13 13:15:32 -0500

    WIP

--- packages/migration-service/src/ledger.ts
+++ packages/migration-service/src/ledger.ts
@@ -1,12 +1,13 @@
 import { daoKey, init } from "@helium/helium-sub-daos-sdk";
-import { AssetProof, getAssetProof, getAssetsByOwner, HNT_MINT } from "@helium/spl-utils";
+import { init as initVsr } from "@helium/voter-stake-registry-sdk";
+import { AssetProof, chunks, getAssetProof, getAssetsByOwner, HNT_MINT } from "@helium/spl-utils";
 import { registrarCollectionKey } from "@helium/voter-stake-registry-sdk";
 import {
   PROGRAM_ID as BUBBLEGUM_PROGRAM_ID,
   createTransferInstruction,
 } from "@metaplex-foundation/mpl-bubblegum";
 import { ConcurrentMerkleTreeAccount, SPL_ACCOUNT_COMPRESSION_PROGRAM_ID, SPL_NOOP_PROGRAM_ID } from "@solana/spl-account-compression";
-import { createTransferCheckedInstruction, getAssociatedTokenAddressSync } from "@solana/spl-token";
+import { createTransferCheckedInstruction, createTransferInstruction as createTokenTransfer, getAssociatedTokenAddressSync, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { AccountMeta, PublicKey, Transaction, TransactionInstruction } from "@solana/web3.js";
 import { provider } from "./solana";
 
@@ -19,6 +20,8 @@ export async function getMigrateTransactions(from: PublicKey, to: PublicKey): Pr
 
   const uniqueAssets = new Set(assets.map(asset => asset.id.toBase58()));
   const hsdProgram = await init(provider);
+  const vsrProgram = await initVsr(provider);
+
   const dao = await hsdProgram.account.daoV0.fetch(daoKey(HNT_MINT)[0]);
   const positionCollection = registrarCollectionKey(dao.registrar)[0];
   const positions = assets.filter(asset => asset.grouping && asset.grouping.equals(positionCollection));
@@ -85,7 +88,61 @@ export async function getMigrateTransactions(from: PublicKey, to: PublicKey): Pr
     }
   }
 
+  const transferPositionIxns = await Promise.all(positions.map(async position => {
+    return await vsrProgram.methods.ledgerTransferPositionV0()
+    .accounts({
+      to,
+      from,
+      mint: position.id,
+    })
+    .instruction()
+  }))
 
+  const tokens = await provider.connection.getParsedTokenAccountsByOwner(from, {
+    programId: TOKEN_PROGRAM_ID,
+  });
+
+  const transferTokenInstructions = tokens.value.filter(token => !uniqueAssets.has(token.account.data.parsed.info.mint)).map(token => {
+    const mint = token.account.data.parsed.info.mint
+    const fromAta = token.pubkey;
+    const toAta = getAssociatedTokenAddressSync(mint, to);
+    return createTokenTransfer(
+      fromAta,
+      toAta,
+      from,
+      token.account.data.parsed.info.tokenAmount.amount,
+    );
+  })
+
+  const recentBlockhash = (await provider.connection.getLatestBlockhash()).blockhash
+  const transactions: Transaction[] = [];
+  chunks(transferAssetIxns, 3).forEach((chunk) => {
+    const tx = new Transaction({
+      feePayer: from,
+      recentBlockhash,
+    });
+    tx.add(...chunk);
+    transactions.push(tx);
+  });
+  
+  if (transferPositionIxns.length > 0) {
+    const tx = new Transaction({
+      feePayer: from,
+      recentBlockhash,
+    });
+    tx.add(...transferAssetIxns);
+    transactions.push(await provider.wallet.signTransaction(tx));
+  }
+  if (transferTokenInstructions.length > 0) {
+    const tx = new Transaction({
+      feePayer: from,
+      recentBlockhash,
+    });
+    tx.add(...transferAssetIxns);
+    transactions.push(tx);
+  }
+
+  return transactions;
 }
 
 const mapProof = (assetProof: AssetProof): AccountMeta[] => {
