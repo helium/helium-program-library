79528e267a64c0dbcb71c5691a657cb559db8fcd
Chewing Glass <chewinglass123@gmail.com>
2023-03-14 10:07:01 -0500

    Increase thread budgets

--- programs/data-credits/src/instructions/mint_data_credits_v0.rs
+++ programs/data-credits/src/instructions/mint_data_credits_v0.rs
@@ -144,13 +144,10 @@ pub fn handler(ctx: Context<MintDataCreditsV0>, args: MintDataCreditsArgsV0) -> 
 
   // Remove the confidence from the price to use the most conservative price
   // https://docs.pyth.network/pythnet-price-feeds/best-practices
-  let hnt_price_with_conf = hnt_price.price.checked_sub(
-    i64::try_from(
-      hnt_price.conf
-        .checked_mul(2)
-        .unwrap()
-    ).unwrap()
-  ).unwrap();
+  let hnt_price_with_conf = hnt_price
+    .price
+    .checked_sub(i64::try_from(hnt_price.conf.checked_mul(2).unwrap()).unwrap())
+    .unwrap();
 
   // dc_exponent = 5 since $1 = 10^5 DC
   // expo is a negative number, i.e. normally -8 for 8 hnt decimals
