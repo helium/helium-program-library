d000f5e41a7836d32e8a99434ea6fa4e49352c7a
ThornM9 <tm.jalal@gmail.com>
2023-03-15 12:48:56 +1000

    contract

---
+++ programs/price-oracle/src/error.rs
@@ -0,0 +1,13 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ErrorCode {
+  #[msg("The realloc increase was too large")]
+  InvalidDataIncrease,
+
+  #[msg("Not authorised to submit a price")]
+  UnauthorisedOracle,
+
+  #[msg("Unable to update price")]
+  InvalidPriceUpdate,
+}
