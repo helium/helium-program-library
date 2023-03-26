9914c82be5ef08aae1d479e8f3801dd15ffee79c
Chewing Glass <chewinglass123@gmail.com>
2023-03-26 17:05:26 -0500

    Rm unused code

--- packages/migration-service/src/ledger.ts
+++ packages/migration-service/src/ledger.ts
@@ -257,21 +257,3 @@ const getBubblegumAuthorityPDA = async (merkleRollPubKey: PublicKey) => {
   );
   return bubblegumAuthorityPDAKey;
 };
-
-const CLOCKWORK_PID = new PublicKey(
-  "CLoCKyJ6DXBJqqu2VWx9RLbgnwwR6BMHHuyasVmfMzBh"
-);
-function threadKey(
-  authority: PublicKey,
-  threadId: string,
-  programId: PublicKey = CLOCKWORK_PID
-): [PublicKey, number] {
-  return PublicKey.findProgramAddressSync(
-    [
-      Buffer.from("thread", "utf8"),
-      authority.toBuffer(),
-      Buffer.from(threadId, "utf8"),
-    ],
-    programId
-  );
-}
