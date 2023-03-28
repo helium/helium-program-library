47853f95ecad8c0c0dde3c099e9bdb911eda7784
Bryan Zettler <bryanzettler@gmail.com>
2023-03-28 16:39:50 -0500

    kill solana file

--- packages/account-postgres-sink-service/src/utils/solana.ts
+++
@@ -1,8 +0,0 @@
-import * as anchor from "@coral-xyz/anchor";
-import { SOLANA_URL } from "../env";
-
-console.log(process.env.ANCHOR_PROVIDER_URL);
-anchor.setProvider(
-  anchor.AnchorProvider.local(process.env.ANCHOR_PROVIDER_URL || SOLANA_URL)
-);
-export const provider = anchor.getProvider() as anchor.AnchorProvider;
