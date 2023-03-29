e82f8652f01bb0456cb72e2cdcca1577eb9dee70
ThornM9 <tm.jalal@gmail.com>
2023-03-29 21:45:35 +1000

    add contract

---
+++ programs/rewards-oracle/src/lib.rs
@@ -0,0 +1,24 @@
+use anchor_lang::prelude::*;
+
+pub mod instructions;
+pub mod state;
+
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("EXCh5WEhHJFJpDz5AiZJ8TrpCZTaLF3KJt1UeXA4yyth");
+
+#[program]
+pub mod rewards_oracle {
+  use super::*;
+
+  pub fn set_current_rewards_wrapper_v0(
+    ctx: Context<SetCurrentRewardsWrapperV0>,
+    args: SetCurrentRewardsWrapperArgsV0,
+  ) -> Result<()> {
+    set_current_rewards_wrapper_v0::handler(ctx, args)
+  }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
