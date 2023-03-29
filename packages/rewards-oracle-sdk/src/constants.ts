cdf70ad20e3fb2df2a96ff4b7a37c57bbc692d3c
ThornM9 <tm.jalal@gmail.com>
2023-03-29 21:50:44 +1000

    add sdk

---
+++ packages/rewards-oracle-sdk/src/constants.ts
@@ -0,0 +1,3 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const PROGRAM_ID = new PublicKey("EXCh5WEhHJFJpDz5AiZJ8TrpCZTaLF3KJt1UeXA4yyth");
--- packages/rewards-oracle-sdk/src/index.ts
+++ packages/rewards-oracle-sdk/src/index.ts
@@ -1,4 +1,4 @@
-import { PriceOracle } from "@helium/idls/lib/types/price_oracle";
+import { RewardsOracle } from "@helium/idls/lib/types/rewards_oracle";
 import { AnchorProvider, Idl, Program } from "@coral-xyz/anchor";
 import { PublicKey } from "@solana/web3.js";
 import { PROGRAM_ID } from "./constants";
@@ -9,17 +9,17 @@ export async function init(
   provider: AnchorProvider,
   programId: PublicKey = PROGRAM_ID,
   idl?: Idl | null
-): Promise<Program<PriceOracle>> {
+): Promise<Program<RewardsOracle>> {
   if (!idl) {
     idl = await Program.fetchIdl(programId, provider);
   }
 
-  const program = new Program<PriceOracle>(
-    idl as PriceOracle,
+  const program = new Program<RewardsOracle>(
+    idl as RewardsOracle,
     programId,
     provider,
     undefined,
-  ) as Program<PriceOracle>;
+  ) as Program<RewardsOracle>;
 
   return program;
 }
