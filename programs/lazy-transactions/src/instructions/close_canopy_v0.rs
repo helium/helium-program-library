1c29fed84f7d16004e7401dab73572c9ba60f575
Chewing Glass <chewinglass123@gmail.com>
2023-04-14 13:43:34 -0500

    Fmt

--- programs/lazy-transactions/src/instructions/close_canopy_v0.rs
+++ programs/lazy-transactions/src/instructions/close_canopy_v0.rs
@@ -2,14 +2,14 @@ use crate::state::*;
 use anchor_lang::{prelude::*, solana_program::system_program};
 
 pub fn close<'info>(info: AccountInfo<'info>, sol_destination: AccountInfo<'info>) -> Result<()> {
-    // Transfer tokens from the account to the sol_destination.
-    let dest_starting_lamports = sol_destination.lamports();
-    **sol_destination.lamports.borrow_mut() =
-        dest_starting_lamports.checked_add(info.lamports()).unwrap();
-    **info.lamports.borrow_mut() = 0;
+  // Transfer tokens from the account to the sol_destination.
+  let dest_starting_lamports = sol_destination.lamports();
+  **sol_destination.lamports.borrow_mut() =
+    dest_starting_lamports.checked_add(info.lamports()).unwrap();
+  **info.lamports.borrow_mut() = 0;
 
-    info.assign(&system_program::ID);
-    info.realloc(0, false).map_err(Into::into)
+  info.assign(&system_program::ID);
+  info.realloc(0, false).map_err(Into::into)
 }
 
 #[derive(Accounts)]
@@ -25,13 +25,14 @@ pub struct CloseCanopyV0<'info> {
   )]
   pub lazy_transactions: Account<'info, LazyTransactionsV0>,
   /// CHECK: Verified by has one
-  #[account(
-    mut,
-  )]
+  #[account(mut)]
   pub canopy: UncheckedAccount<'info>,
 }
 
 pub fn handler(ctx: Context<CloseCanopyV0>) -> Result<()> {
-  close(ctx.accounts.canopy.to_account_info(), ctx.accounts.refund.to_account_info())?;
+  close(
+    ctx.accounts.canopy.to_account_info(),
+    ctx.accounts.refund.to_account_info(),
+  )?;
   Ok(())
 }
