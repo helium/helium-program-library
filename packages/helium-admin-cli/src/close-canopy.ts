eecddd44ed4fa971f22b2abbedbb8c2e002959c6
Chewing Glass <chewinglass123@gmail.com>
2023-04-17 09:58:57 -0500

    Add close canopy

---
+++ packages/helium-admin-cli/src/close-canopy.ts
@@ -0,0 +1,74 @@
+import * as anchor from "@coral-xyz/anchor";
+import { daoKey, init as initHsd } from "@helium/helium-sub-daos-sdk";
+import { PublicKey } from "@solana/web3.js";
+import Squads from "@sqds/sdk";
+import os from "os";
+import yargs from "yargs/yargs";
+import { parseEmissionsSchedule, sendInstructionsOrSquads } from "./utils";
+import { init, lazyTransactionsKey } from "@helium/lazy-transactions-sdk";
+
+export async function run(args: any = process.argv) {
+  const yarg = yargs(args).options({
+    wallet: {
+      alias: "k",
+      describe: "Anchor wallet keypair",
+      default: `${os.homedir()}/.config/solana/id.json`,
+    },
+    url: {
+      alias: "u",
+      default: "http://127.0.0.1:8899",
+      describe: "The solana url",
+    },
+    name: {
+      required: true,
+      type: "string",
+      describe: "Lazy dist name",
+    },
+    executeTransaction: {
+      type: "boolean",
+    },
+    multisig: {
+      type: "string",
+      describe:
+        "Address of the squads multisig to be authority. If not provided, your wallet will be the authority",
+    },
+    authorityIndex: {
+      type: "number",
+      describe: "Authority index for squads. Defaults to 1",
+      default: 1,
+    },
+  });
+  const argv = await yarg.argv;
+  process.env.ANCHOR_WALLET = argv.wallet;
+  process.env.ANCHOR_PROVIDER_URL = argv.url;
+  anchor.setProvider(anchor.AnchorProvider.local(argv.url));
+  const provider = anchor.getProvider() as anchor.AnchorProvider;
+  const program = await init(provider);
+
+  const instructions = [];
+
+  const lazyTransactions = lazyTransactionsKey(argv.name)[0];
+  instructions.push(
+    await program.methods.closeCanopyV0().accounts({
+      lazyTransactions,
+      refund: provider.wallet.publicKey
+    }).instruction()
+  )
+
+  const squads = Squads.endpoint(
+    process.env.ANCHOR_PROVIDER_URL,
+    provider.wallet,
+    {
+      commitmentOrConfig: "finalized",
+    }
+  );
+  await sendInstructionsOrSquads({
+    provider,
+    instructions,
+    executeTransaction: argv.executeTransaction,
+    squads,
+    multisig: argv.multisig ? new PublicKey(argv.multisig) : undefined,
+    authorityIndex: argv.authorityIndex,
+    signers: [],
+  });
+}
