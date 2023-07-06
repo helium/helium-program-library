3ec46f7c65dd8e1f89ec9b88eed057ea09dca5d2
ThornM9 <tm.jalal@gmail.com>
2023-07-06 14:01:36 +1000

    fix audit findings

---
+++ programs/helium-entity-manager/src/instructions/update_program_tree_v0.rs
@@ -0,0 +1,105 @@
+use crate::{program_address, state::*};
+use anchor_lang::prelude::*;
+use mpl_bubblegum::{
+  cpi::{accounts::CreateTree, create_tree},
+  program::Bubblegum,
+};
+use shared_utils::resize_to_fit;
+use spl_account_compression::{program::SplAccountCompression, Noop};
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct UpdateProgramTreeArgsV0 {
+  pub max_depth: u32,
+  pub max_buffer_size: u32,
+  pub approver_seeds: Vec<Vec<u8>>,
+  // This merkle tree gets removed if supplied, the approved program has the responsibility of making sure they aren't kicking out a valid tree
+  pub old_merkle_tree: Option<Pubkey>,
+}
+
+#[derive(Accounts)]
+#[instruction(args: UpdateProgramTreeArgsV0)]
+pub struct UpdateProgramTreeV0<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
+  #[account(
+    address = program_address(args.approver_seeds, &program_approval.program_id)?
+  )]
+  pub program_approver: Signer<'info>,
+  pub program_approval: Box<Account<'info, ProgramApprovalV0>>,
+  #[account(
+    mut,
+    seeds = [new_merkle_tree.key().as_ref()],
+    bump,
+    seeds::program = bubblegum_program.key()
+  )]
+  /// CHECK: Checked by cpi
+  pub new_tree_authority: UncheckedAccount<'info>,
+  #[account(mut)]
+  /// CHECK: Checked by cpi
+  pub new_merkle_tree: AccountInfo<'info>,
+
+  pub tree_creator: Signer<'info>,
+
+  pub log_wrapper: Program<'info, Noop>,
+  pub system_program: Program<'info, System>,
+  pub bubblegum_program: Program<'info, Bubblegum>,
+  pub compression_program: Program<'info, SplAccountCompression>,
+}
+
+pub fn handler(ctx: Context<UpdateProgramTreeV0>, args: UpdateProgramTreeArgsV0) -> Result<()> {
+  create_tree(
+    CpiContext::new(
+      ctx.accounts.bubblegum_program.to_account_info().clone(),
+      CreateTree {
+        tree_authority: ctx.accounts.new_tree_authority.to_account_info().clone(),
+        merkle_tree: ctx.accounts.new_merkle_tree.to_account_info().clone(),
+        payer: ctx.accounts.payer.to_account_info().clone(),
+        tree_creator: ctx.accounts.tree_creator.to_account_info().clone(),
+        log_wrapper: ctx.accounts.log_wrapper.to_account_info().clone(),
+        compression_program: ctx.accounts.compression_program.to_account_info().clone(),
+        system_program: ctx.accounts.system_program.to_account_info().clone(),
+      },
+    ),
+    args.max_depth,
+    args.max_buffer_size,
+    None,
+  )?;
+  match args.old_merkle_tree {
+    Some(old_merkle_tree) => {
+      let index = ctx
+        .accounts
+        .program_approval
+        .approved_merkle_trees
+        .iter()
+        .position(|&x| x == old_merkle_tree);
+      match index {
+        Some(idx) => {
+          ctx.accounts.program_approval.approved_merkle_trees[idx] =
+            ctx.accounts.new_merkle_tree.key();
+        }
+        None => {
+          ctx
+            .accounts
+            .program_approval
+            .approved_merkle_trees
+            .push(ctx.accounts.new_merkle_tree.key());
+        }
+      }
+    }
+    None => {
+      ctx
+        .accounts
+        .program_approval
+        .approved_merkle_trees
+        .push(ctx.accounts.new_merkle_tree.key());
+    }
+  }
+
+  resize_to_fit(
+    &ctx.accounts.payer.to_account_info(),
+    &ctx.accounts.system_program.to_account_info(),
+    &ctx.accounts.program_approval,
+  )?;
+
+  Ok(())
+}
--- programs/mobile-entity-manager/src/instructions/update_carrier_tree_v0.rs
+++ programs/mobile-entity-manager/src/instructions/update_carrier_tree_v0.rs
@@ -1,11 +1,11 @@
 use crate::error::ErrorCode;
 use crate::{carrier_seeds, state::*};
 use anchor_lang::prelude::*;
-use mpl_bubblegum::{
-  cpi::{accounts::CreateTree, create_tree},
-  program::Bubblegum,
-  state::TreeConfig,
+use helium_entity_manager::{
+  cpi::accounts::UpdateProgramTreeV0, cpi::update_program_tree_v0, program::HeliumEntityManager,
+  ProgramApprovalV0, UpdateProgramTreeArgsV0,
 };
+use mpl_bubblegum::{program::Bubblegum, state::TreeConfig};
 use spl_account_compression::{program::SplAccountCompression, Noop};
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
@@ -21,6 +21,10 @@ pub struct UpdateCarrierTreeV0<'info> {
   pub payer: Signer<'info>,
   #[account(mut)]
   pub carrier: Box<Account<'info, CarrierV0>>,
+  #[account(
+    constraint = program_approval.program_id == crate::id(),
+  )]
+  pub program_approval: Box<Account<'info, ProgramApprovalV0>>,
   /// CHECK: Conditionally decoded
   #[account(
     mut,
@@ -45,6 +49,7 @@ pub struct UpdateCarrierTreeV0<'info> {
   pub system_program: Program<'info, System>,
   pub bubblegum_program: Program<'info, Bubblegum>,
   pub compression_program: Program<'info, SplAccountCompression>,
+  pub helium_entity_manager_program: Program<'info, HeliumEntityManager>,
 }
 
 pub fn handler(ctx: Context<UpdateCarrierTreeV0>, args: UpdateCarrierTreeArgsV0) -> Result<()> {
@@ -60,24 +65,39 @@ pub fn handler(ctx: Context<UpdateCarrierTreeV0>, args: UpdateCarrierTreeArgsV0)
     );
   }
 
-  create_tree(
+  let old_merkle_tree = if ctx.accounts.carrier.merkle_tree == Pubkey::default() {
+    None
+  } else {
+    Some(ctx.accounts.carrier.merkle_tree)
+  };
+
+  let seeds: &[&[&[u8]]] = &[carrier_seeds!(ctx.accounts.carrier)];
+
+  update_program_tree_v0(
     CpiContext::new_with_signer(
       ctx.accounts.bubblegum_program.to_account_info().clone(),
-      CreateTree {
-        tree_authority: ctx.accounts.new_tree_authority.to_account_info().clone(),
-        merkle_tree: ctx.accounts.new_merkle_tree.to_account_info().clone(),
+      UpdateProgramTreeV0 {
         payer: ctx.accounts.payer.to_account_info().clone(),
+        program_approver: ctx.accounts.carrier.to_account_info().clone(),
+        program_approval: ctx.accounts.program_approval.to_account_info().clone(),
+        new_tree_authority: ctx.accounts.new_tree_authority.to_account_info().clone(),
+        new_merkle_tree: ctx.accounts.new_merkle_tree.to_account_info().clone(),
         tree_creator: ctx.accounts.carrier.to_account_info().clone(),
         log_wrapper: ctx.accounts.log_wrapper.to_account_info().clone(),
+        bubblegum_program: ctx.accounts.bubblegum_program.to_account_info().clone(),
         compression_program: ctx.accounts.compression_program.to_account_info().clone(),
         system_program: ctx.accounts.system_program.to_account_info().clone(),
       },
       signer_seeds,
     ),
-    args.max_depth,
-    args.max_buffer_size,
-    None,
+    UpdateProgramTreeArgsV0 {
+      max_depth: args.max_depth,
+      max_buffer_size: args.max_buffer_size,
+      old_merkle_tree,
+      approver_seeds: seeds[0].iter().map(|s| s.to_vec()).collect(),
+    },
   )?;
+
   ctx.accounts.carrier.merkle_tree = ctx.accounts.new_merkle_tree.key();
 
   Ok(())
