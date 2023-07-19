352305395c702ec491fae1897be410ccbb7387f2
Chewing Glass <chewinglass123@gmail.com>
2023-07-18 21:10:13 -0500

    Fix use vote hook

--- packages/voter-stake-registry-hooks/src/hooks/useVote.ts
+++ packages/voter-stake-registry-hooks/src/hooks/useVote.ts
@@ -1,43 +1,52 @@
-import { BN } from "@coral-xyz/anchor";
 import { useAnchorProvider } from "@helium/helium-react-hooks";
-import { sendInstructions } from "@helium/spl-utils";
-import { init, positionKey } from "@helium/voter-stake-registry-sdk";
 import {
-  MintLayout,
-  createInitializeMintInstruction,
-  TOKEN_PROGRAM_ID,
-} from "@solana/spl-token";
-import {
-  Keypair,
-  PublicKey,
-  SystemProgram,
-  TransactionInstruction,
-} from "@solana/web3.js";
-import { useAsync, useAsyncCallback } from "react-async-hook";
-import { HeliumVsrClient } from "../sdk/client";
-import { getRegistrarKey } from "../utils/getPositionKeys";
+  bulkSendTransactions,
+  chunks
+} from "@helium/spl-utils";
+import { init } from "@helium/voter-stake-registry-sdk";
+import { PublicKey } from "@metaplex-foundation/js";
+import { Transaction } from "@solana/web3.js";
+import { useAsyncCallback } from "react-async-hook";
 import { useHeliumVsrState } from "../contexts/heliumVsrContext";
 
 export const useVote = () => {
   const provider = useAnchorProvider();
   const { positions } = useHeliumVsrState();
   const { error, loading, execute } = useAsyncCallback(
-    async ({
-      choice
-    }: {
-      choice: number
-    }) => {
+    async ({ choice, proposal }: { choice: number; proposal: PublicKey }) => {
       const isInvalid = !provider || !positions || positions.length === 0;
 
       if (isInvalid) {
-        throw new Error("Unable to vote without positions. Please stake tokens first.");
+        throw new Error(
+          "Unable to vote without positions. Please stake tokens first."
+        );
       } else {
         const vsrProgram = await init(provider);
-        const instructions = await Promise.all(positions.map(async position => {
-          await vsrProgram.methods.voteV0
-        }))
+        const instructions = await Promise.all(
+          positions.map(async (position) => {
+            return await vsrProgram.methods
+              .voteV0({
+                choice,
+              })
+              .accounts({
+                proposal,
+                voter: provider.wallet.publicKey,
+                position: position.pubkey,
+              })
+              .instruction();
+          })
+        );
 
-        await sendInstructions(provider, instructions, [mintKeypair]);
+        const txs = chunks(instructions, 4).map((ixs) => {
+          const tx = new Transaction({
+            feePayer: provider.wallet.publicKey,
+          });
+          tx.add(...ixs);
+
+          return tx;
+        });
+
+        await bulkSendTransactions(provider, txs);
       }
     }
   );
