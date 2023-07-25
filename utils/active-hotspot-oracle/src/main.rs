9d57c607727f054d069d3f1632bfd8ca03f462b0
ThornM9 <tm.jalal@gmail.com>
2023-07-25 14:31:57 +1000

    fixes

--- utils/active-hotspot-oracle/src/main.rs
+++ utils/active-hotspot-oracle/src/main.rs
@@ -10,7 +10,6 @@ use deltalake::{datafusion::prelude::SessionContext, DeltaTableBuilder};
 use helium_entity_manager::{accounts::SetEntityActiveV0, SetEntityActiveArgsV0};
 use helium_entity_manager::{IotHotspotInfoV0, MobileHotspotInfoV0};
 use hpl_utils::{dao::SubDao, send_and_confirm_messages_with_spinner};
-use indicatif::{ProgressBar, ProgressStyle};
 use s3::{bucket::Bucket, creds::Credentials, region::Region};
 use serde::{Deserialize, Serialize};
 use solana_client::tpu_client::{TpuClient, TpuClientConfig};
@@ -41,7 +40,7 @@ struct Args {
   #[clap(long)]
   pub aws_region: Option<String>,
 
-  /// Should only be used for local testing, set as environment variables in deployment
+  /// Should only be used for local testing
   #[clap(long)]
   pub access_key_id: Option<String>,
   #[clap(long)]
@@ -57,11 +56,15 @@ struct Args {
   /// Checks that all hotspots are marked correctly on-chain
   #[clap(long, short, action)]
   check_validity: bool,
+
+  /// Runs the oracle without sending any transactions. Will still update S3 checkpoints.
+  #[clap(long, short, action)]
+  dry_run: bool,
 }
 
 #[derive(Serialize, Deserialize)]
 struct ActiveCheckpoint {
-  set: HashSet<String>,
+  entity_keys: HashSet<String>,
 }
 
 fn load_arg_from_cli_or_env(cli_opt: Option<String>, env_name: &str) -> String {
@@ -158,7 +161,7 @@ async fn main() -> Result<()> {
   let current_time = Utc::now();
   let thirty_days_ago = current_time - Duration::days(30);
   let query = format!(
-    "SELECT * FROM delta_table WHERE most_recent_ts > {}",
+    "SELECT * FROM delta_table WHERE most_recent_ts > {} LIMIT 1000",
     thirty_days_ago.timestamp(),
   );
   let record_batches = session.sql(query.as_str()).await?.collect().await?;
@@ -214,6 +217,7 @@ async fn main() -> Result<()> {
     &tpu_client,
     mark_active_iot_ixs,
     &kp,
+    args.dry_run,
   )?;
   println!("Sending mark active mobile transactions");
   construct_and_send_txs(
@@ -221,6 +225,7 @@ async fn main() -> Result<()> {
     &tpu_client,
     mark_active_mobile_ixs,
     &kp,
+    args.dry_run,
   )?;
   println!("Sending mark inactive iot transactions");
   construct_and_send_txs(
@@ -228,6 +233,7 @@ async fn main() -> Result<()> {
     &tpu_client,
     mark_inactive_iot_ixs,
     &kp,
+    args.dry_run,
   )?;
   println!("Sending mark inactive mobile transactions");
   construct_and_send_txs(
@@ -235,6 +241,7 @@ async fn main() -> Result<()> {
     &tpu_client,
     mark_inactive_mobile_ixs,
     &kp,
+    args.dry_run,
   )?;
 
   // write to new checkpoint file and upload to s3
@@ -275,11 +282,17 @@ fn construct_and_send_txs(
   tpu_client: &TpuClient,
   ixs: Vec<Instruction>,
   payer: &Keypair,
+  dry_run: bool,
 ) -> Result<()> {
   // send transactions in batches so blockhash doesn't expire
   let transaction_batch_size = 100;
-  let ixs_per_tx = 28;
-  let mut tx_count = 0;
+  let ixs_per_tx = 10;
+  println!("Ixs to execute: {}", ixs.len());
+  println!(
+    "Txs to execute: {}",
+    (ixs.len() + ixs_per_tx - 1) / ixs_per_tx
+  );
+
   for i in (0..ixs.len()).step_by(transaction_batch_size * ixs_per_tx) {
     let tx_start = i;
     let tx_end = (i + transaction_batch_size * ixs_per_tx).min(ixs.len());
@@ -294,8 +307,10 @@ fn construct_and_send_txs(
         .map_err(|e| anyhow!("Error while signing tx: {e}"))?;
       txs.push(tx);
     }
-    tx_count += txs.len();
 
+    if dry_run {
+      continue;
+    }
     let serialized_txs = txs
       .iter()
       .map(|tx| bincode::serialize(&tx.clone()).map_err(|e| anyhow!("Failed to serialize tx: {e}")))
@@ -309,8 +324,6 @@ fn construct_and_send_txs(
     )
     .context("Failed sending transactions")?;
   }
-  println!("ix_count: {}", ixs.len());
-  println!("tx_count: {}", tx_count);
 
   Ok(())
 }
@@ -426,16 +439,11 @@ fn find_infos_to_mark(
   let mut valid_iot_infos = Vec::new();
   let mut valid_mobile_infos = Vec::new();
   let step = 100;
-  let progress_bar = ProgressBar::new(entity_keys.len().try_into().unwrap());
-  progress_bar.set_style(
-    ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
-      .unwrap()
-      .progress_chars("##-"),
-  );
-  progress_bar.set_message("Fetching accounts...");
 
   for i in (0..entity_keys.len()).step_by(step) {
-    progress_bar.set_position(i.try_into().unwrap());
+    if (i / step) % 50 == 0 {
+      println!("{} / {}", i, entity_keys.len());
+    }
     let start = i;
     let end = (i + step).min(entity_keys.len());
     let iot_infos = &mark_iot_infos[start..end];
@@ -512,8 +520,7 @@ fn find_infos_to_mark(
       }
     }
   }
-  progress_bar.finish_with_message("Finished fetching accounts");
-
+  println!("Finished fetching infos");
   return Ok((valid_iot_infos, valid_mobile_infos));
 }
 
@@ -522,19 +529,35 @@ async fn save_checkpoint(
   s3_config: HashMap<String, String>,
   file_name: &str,
 ) -> Result<()> {
-  let checkpoint = ActiveCheckpoint { set: hashset };
+  let checkpoint = ActiveCheckpoint {
+    entity_keys: hashset,
+  };
   let json_data = serde_json::to_string(&checkpoint).context("Failed to serialize data")?;
 
   let bucket_name = s3_config.get("bucket_name").unwrap();
   let bucket = create_bucket(s3_config.clone()).context("Failed to create s3 bucket object")?;
-  bucket
-    .put_object(
-      format!("/{}/{}", bucket_name, file_name),
-      json_data.as_bytes(),
-    )
-    .await?;
+  let path = get_checkpoint_path(s3_config.clone(), bucket_name, file_name);
+  bucket.put_object(path, json_data.as_bytes()).await?;
 
   Ok(())
+}
+
+fn get_checkpoint_path(
+  s3_config: HashMap<String, String>,
+  bucket_name: &String,
+  file_name: &str,
+) -> String {
+  // on local minio server, you also need to specify the bucket name in the file path
+  let path = if let Some(access_key) = s3_config.get("aws_access_key_id") {
+    if access_key.contains("minio") {
+      format!("/{}/checkpoints/{}", bucket_name, file_name)
+    } else {
+      format!("/checkpoints/{}", file_name)
+    }
+  } else {
+    format!("/checkpoints/{}", file_name)
+  };
+  path
 }
 
 async fn load_checkpoint(
@@ -543,30 +566,45 @@ async fn load_checkpoint(
 ) -> Result<HashSet<String>> {
   let bucket_name = s3_config.get("bucket_name").unwrap();
   let bucket = create_bucket(s3_config.clone()).context("Failed to create s3 bucket object")?;
-  let file_contents_buffer = bucket
-    .get_object(format!("/{}/{}", bucket_name, file_name))
-    .await?;
+  let path = get_checkpoint_path(s3_config.clone(), bucket_name, file_name);
+  let file_contents_buffer = bucket.get_object(path).await?;
 
   let checkpoint: ActiveCheckpoint = serde_json::from_slice(&file_contents_buffer.as_slice())
     .context("Failed to deserialize checkpoint")?;
 
-  Ok(checkpoint.set)
+  Ok(checkpoint.entity_keys)
 }
 
 fn create_bucket(s3_config: HashMap<String, String>) -> Result<Bucket> {
   // Set up AWS credentials and region
-  let region: Region;
   let endpoint_opt = s3_config.get("aws_endpoint");
   let region_str = s3_config.get("aws_default_region").unwrap().clone();
-  if endpoint_opt.is_some() {
-    region = Region::Custom {
+  let region = if let Some(endpoint) = endpoint_opt {
+    Region::Custom {
       region: region_str,
-      endpoint: s3_config.get("aws_endpoint").unwrap().clone(),
-    };
+      endpoint: endpoint.clone(),
+    }
   } else {
-    region = Region::from_str(&region_str).unwrap();
-  }
-  let credentials = Credentials::new(None, None, None, None, None)?;
+    Region::from_str(&region_str).unwrap()
+  };
+  let aws_access_key_opt = s3_config.get("aws_access_key_id");
+  let aws_secret_key_opt = s3_config.get("aws_secret_access_key");
+  let credentials = if let (Some(aws_access_key), Some(aws_secret_key)) =
+    (aws_access_key_opt, aws_secret_key_opt)
+  {
+    println!("aws access key: {}", aws_access_key);
+    println!("aws secret key: {}", aws_secret_key);
+    Credentials::new(
+      Some(aws_access_key.clone().as_str()),
+      Some(aws_secret_key.clone().as_str()),
+      None,
+      None,
+      None,
+    )?
+  } else {
+    Credentials::new(None, None, None, None, None)?
+  };
+
   let bucket_name = s3_config.get("bucket_name").unwrap();
 
   Ok(Bucket::new(bucket_name, region, credentials)?)
