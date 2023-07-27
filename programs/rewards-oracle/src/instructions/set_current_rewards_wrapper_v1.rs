f1c4cd9572e74cf903cc94144d16d2e2d0d5d917
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2023-07-27 12:38:20 -0500

    Enable large RSA keys with keyToAsset creator on all new assets, and v1 endpoint. (#337)
    * Fix claim rewards
    
    * Unpin mpl (#305)
    
    * Unpin mpl
    
    * Fix claim rewards
    
    * Fix build
    
    * v0.2.10
    
    * fix audit findings
    
    * add test
    
    * clippy
    
    * undo cpi change
    
    * fix test
    
    * add check
    
    * add utility script for generating add gateway txs
    
    * Tracking hotspot onboarding fees and active status on-chain  (#299)
    
    * contract changes
    
    * delete old code
    
    * fixes + tests
    
    * restrict pda signer
    
    * fix security issue
    
    * fix tests
    
    * macros
    
    * Actions for checking docker and rust builds succeed (#319)
    
    * add docker and rust build actions
    
    * debug
    
    * fix
    
    * docker setup
    
    * try
    
    * try latest ubuntu
    
    * add more tests and fix ecc verifier build
    
    * fix more broken builds
    
    * rm
    
    * fix (#318)
    
    * Audit fixes (#317)
    
    * Audit fixes
    
    * Use saturating sub
    
    * clippy
    
    ---------
    
    Co-authored-by: ThornM9 <tm.jalal@gmail.com>
    
    * Fix resize
    
    * Upgrade helium-crypto (#316)
    
    * Upgrade helium-crypto
    
    * Review feedback, crypto bump
    
    * jsonParsed instead of JsonParsed on webhooks
    
    * Improve react hooks (#315)
    
    * Improve react hooks
    
    * CR comments
    
    * rm unknown clippy
    
    * Revert "rm unknown clippy"
    
    This reverts commit 9f5326e033101ecb6512bce8bd69a53797c428dd.
    
    * v0.2.11
    
    * Fix pg sink compile
    
    * fix account webhooks
    
    * Rm log
    
    * Remove migration verification, all hotspots migrated (#322)
    
    * add back temp mobile discriminator fix
    
    * Revert "add back temp mobile discriminator fix"
    
    This reverts commit 86e9c43450b6f0b16690b7f1db9f6c3f4c89ff20.
    
    * add endpoint to pay for mobile onboarding fees (#314)
    
    * add endpoint to pay for mobile onboarding fees
    
    * mark as temp
    
    * review comments
    
    * add test
    
    ---------
    
    Co-authored-by: Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
    
    * Add script and endpoint to update metadata (#324)
    
    * Add script and endpoint to update metadata
    
    * fmt
    
    * trigger hsd build
    
    * Backfill mobile onboarding info (#320)
    
    * add backfill for onboarding fees
    
    * snapshot
    
    * lint
    
    ---------
    
    Co-authored-by: Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
    
    * Add separate data only script (#333)
    
    * add script
    
    * rm unnecessary imports
    
    * integrity check and metric (#332)
    
    * integrity check and metric
    
    * integrity checking and updating
    
    * update configs file
    
    * fix typo and duplicate fns
    
    * Clippy fix
    
    * Remove mpl-bubblegum and account-compression dependencies, replace wi… (#331)
    
    * Remove mpl-bubblegum and account-compression dependencies, replace with anchor-gen
    
    * Cargo fmt
    
    * fix registering plugins (#335)
    
    * fix registering plugins
    
    * Add type to integrityMetric
    
    * wrap all in async
    
    * Remove fastify-plugin
    
    * return array instead of null
    
    * Fix types
    
    * Fix issues in account-post-gres-sink
    
    * WIP
    
    * WIP
    
    * Tests pass. Correct going forward. Still need backfill
    
    * Fix distributor oracle tests
    
    * Remove mpl-bubblegum and account-compression dependencies, replace with anchor-gen
    
    * Cargo fmt
    
    * Add key to asset as second creator to all fut NFTs. Use with priority on oracle
    
    * Fix metadata service
    
    * Rm needless log
    
    * Merge with master
    
    * Appease clippy
    
    * Review comments
    
    * Fix build
    
    ---------
    
    Co-authored-by: ThornM9 <tm.jalal@gmail.com>
    Co-authored-by: ThornM9 <31262123+ThornM9@users.noreply.github.com>
    Co-authored-by: Marc Nijdam <marc_nijdam@mac.com>
    Co-authored-by: Bryan <bryanzettler@gmail.com>

---
+++ programs/rewards-oracle/src/instructions/set_current_rewards_wrapper_v1.rs
@@ -0,0 +1,77 @@
+use anchor_lang::prelude::*;
+use helium_entity_manager::state::*;
+use lazy_distributor::program::LazyDistributor;
+use lazy_distributor::state::*;
+use lazy_distributor::{
+  cpi::{accounts::SetCurrentRewardsV0, set_current_rewards_v0},
+  SetCurrentRewardsArgsV0,
+};
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct SetCurrentRewardsWrapperArgsV1 {
+  pub oracle_index: u16,
+  pub current_rewards: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(args: SetCurrentRewardsWrapperArgsV1)]
+pub struct SetCurrentRewardsWrapperV1<'info> {
+  #[account(mut)]
+  // the oracle EOA that gets wrapped
+  pub oracle: Signer<'info>,
+  pub lazy_distributor: Box<Account<'info, LazyDistributorV0>>,
+  #[account(
+    mut,
+    has_one = lazy_distributor
+  )]
+  pub recipient: Box<Account<'info, RecipientV0>>,
+
+  // We assume that the oracle verified that the key to asset entity key went with these rewards.
+  // This endpoint then verifies the connection of that key_to_asset to the recipient asset.
+  #[account(
+    constraint = key_to_asset.asset == recipient.asset,
+  )]
+  pub key_to_asset: Box<Account<'info, KeyToAssetV0>>,
+
+  /// CHECK: checked in cpi
+  #[account(
+    seeds = ["oracle_signer".as_bytes()],
+    bump
+  )]
+  pub oracle_signer: AccountInfo<'info>,
+  pub lazy_distributor_program: Program<'info, LazyDistributor>,
+  pub system_program: Program<'info, System>,
+}
+
+pub fn handler(
+  ctx: Context<SetCurrentRewardsWrapperV1>,
+  args: SetCurrentRewardsWrapperArgsV1,
+) -> Result<()> {
+  let mut approver = ctx.accounts.oracle_signer.to_account_info().clone();
+  approver.is_signer = true;
+  let cpi_accounts = SetCurrentRewardsV0 {
+    payer: ctx.accounts.oracle.to_account_info(),
+    lazy_distributor: ctx.accounts.lazy_distributor.to_account_info(),
+    recipient: ctx.accounts.recipient.to_account_info(),
+    oracle: ctx.accounts.oracle.to_account_info(),
+    system_program: ctx.accounts.system_program.to_account_info(),
+  };
+
+  let signer_seeds: &[&[&[u8]]] = &[&[
+    "oracle_signer".as_bytes(),
+    &[*ctx.bumps.get("oracle_signer").unwrap()],
+  ]];
+  set_current_rewards_v0(
+    CpiContext::new_with_signer(
+      ctx.accounts.lazy_distributor_program.to_account_info(),
+      cpi_accounts,
+      signer_seeds,
+    )
+    .with_remaining_accounts(vec![approver]),
+    SetCurrentRewardsArgsV0 {
+      oracle_index: args.oracle_index,
+      current_rewards: args.current_rewards,
+    },
+  )?;
+  Ok(())
+}
