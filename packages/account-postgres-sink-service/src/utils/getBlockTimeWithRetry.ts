e29d4d283874503ff8363548685dae3c4e270bdb
Bryan <bryanzettler@gmail.com>
2023-08-08 11:53:24 -0500

    add better logging to integrity-check (#359)
    * add better logging to integrity-check
    
    * fix configs example
    
    * typo
    
    * Fix build errors

---
+++ packages/account-postgres-sink-service/src/utils/getBlockTimeWithRetry.ts
@@ -0,0 +1,37 @@
+import * as anchor from '@coral-xyz/anchor';
+
+export const getBlockTimeWithRetry = async ({
+  slot,
+  maxRetries = 3,
+  retryInterval = 1000,
+  provider,
+}: {
+  slot: number;
+  maxRetries?: number;
+  retryInterval?: number;
+  provider: anchor.AnchorProvider;
+}): Promise<number | null> => {
+  try {
+    const connection = provider.connection;
+
+    // Get the block time of the specified slot
+    const blockTime = await connection.getBlockTime(slot);
+
+    return blockTime;
+  } catch (error) {
+    console.error('Error fetching block time:', error);
+
+    if (maxRetries > 0) {
+      console.log(`Retrying in ${retryInterval / 1000} seconds...`);
+      await new Promise((resolve) => setTimeout(resolve, retryInterval));
+      return getBlockTimeWithRetry({
+        slot,
+        maxRetries: maxRetries - 1,
+        retryInterval,
+        provider,
+      });
+    } else {
+      throw new Error('Max retries reached. Unable to fetch block time.');
+    }
+  }
+};
