e5880765b297768d3f233f0505258026eccc0b3a
bry <bryanzettler@gmail.com>
2023-08-23 11:19:36 -0500

    export getAssets

--- packages/spl-utils/src/index.ts
+++ packages/spl-utils/src/index.ts
@@ -1,23 +1,24 @@
-export * from "./extendBorsh";
-export * from "./transaction";
-export * from "./anchorError";
-export * from "./executeRemoteTxn";
-export * from "./utils";
-export * from "./token";
-export * from "./constants";
+export * from './extendBorsh';
+export * from './transaction';
+export * from './anchorError';
+export * from './executeRemoteTxn';
+export * from './utils';
+export * from './token';
+export * from './constants';
 
 export type {
   AssetProof,
   Asset,
   AssetsByOwnerOpts,
   SearchAssetsOpts,
-} from "./mplAssetAPI";
+} from './mplAssetAPI';
 export {
   getAsset,
+  getAssets,
   getAssetProof,
   getAssetsByOwner,
   searchAssets,
-} from "./mplAssetAPI";
+} from './mplAssetAPI';
 
-export { proofArgsAndAccounts } from "./proofArgsAndAccounts";
-export type { ProofArgsAndAccountsArgs } from "./proofArgsAndAccounts";
+export { proofArgsAndAccounts } from './proofArgsAndAccounts';
+export type { ProofArgsAndAccountsArgs } from './proofArgsAndAccounts';
