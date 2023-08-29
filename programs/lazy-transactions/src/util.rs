fae7c04b251b1ad75f9e5e391a3864c16f453242
Chewing Glass <chewinglass123@gmail.com>
2023-08-29 16:09:32 -0500

    -Appease clippy

--- programs/lazy-transactions/src/util.rs
+++ programs/lazy-transactions/src/util.rs
@@ -4,13 +4,13 @@ pub fn get_bitmap_len(max_depth: u32) -> usize {
   ((num_txns + 8 - 1) / 8) as usize
 }
 
-pub fn is_executed(bitmap: &Vec<u8>, index: u32) -> bool {
+pub fn is_executed(bitmap: &[u8], index: u32) -> bool {
   let byte = bitmap[index as usize / 8];
   let bit = 1 << (index % 8);
   byte & bit != 0
 }
 
-pub fn set_executed(bitmap: &mut Vec<u8>, index: u32) {
+pub fn set_executed(bitmap: &mut [u8], index: u32) {
   let byte = &mut bitmap[index as usize / 8];
   let bit = 1 << (index % 8);
   *byte |= bit;
