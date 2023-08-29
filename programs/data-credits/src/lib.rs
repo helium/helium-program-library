24493e27699e03f2a5dd6ce127dfb5812867ef61
Chewing Glass <chewinglass123@gmail.com>
2023-08-29 17:12:34 -0500

    chore(#307, #378): Add security.txt and verified builds

--- programs/data-credits/src/lib.rs
+++ programs/data-credits/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+#[cfg(not(feature = "no-entrypoint"))]
+use {default_env::default_env, solana_security_txt::security_txt};
 
 pub mod circuit_breaker;
 pub mod errors;
@@ -10,6 +12,20 @@ pub use instructions::*;
 pub use state::*;
 
 declare_id!("credMBJhYFzfn7NxBMdU4aUqFggAjgztaCcv2Fo6fPT");
+
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+  name: "Data Credits",
+  project_url: "http://helium.com",
+  contacts: "email:hello@helium.foundation",
+
+  // Optional Fields
+  preferred_languages: "en",
+  source_code: "https://github.com/helium/helium-program-library/tree/master/programs/data-credits",
+  source_revision: default_env!("GITHUB_SHA", ""),
+  source_release: default_env!("GITHUB_REF_NAME", ""),
+  auditors: "Sec3"
+}
 
 #[program]
 pub mod data_credits {
--- programs/fanout/src/lib.rs
+++ programs/fanout/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+#[cfg(not(feature = "no-entrypoint"))]
+use {default_env::default_env, solana_security_txt::security_txt};
 
 declare_id!("fanqeMu3fw8R4LwKNbahPtYXJsyLL6NXyfe2BqzhfB6");
 
@@ -9,6 +11,20 @@ pub mod state;
 pub use errors::*;
 pub use instructions::*;
 pub use state::*;
+
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+  name: "Fanout",
+  project_url: "http://helium.com",
+  contacts: "email:hello@helium.foundation",
+
+  // Optional Fields
+  preferred_languages: "en",
+  source_code: "https://github.com/helium/helium-program-library/tree/master/programs/fanout",
+  source_revision: default_env!("GITHUB_SHA", ""),
+  source_release: default_env!("GITHUB_REF_NAME", ""),
+  auditors: "Sec3"
+}
 
 #[program]
 pub mod fanout {
