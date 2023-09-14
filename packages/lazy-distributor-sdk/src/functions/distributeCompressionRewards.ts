c70e7c9ad2fbdae5f62a2968de705b53a891a46e
Chewing Glass <chewinglass123@gmail.com>
2023-09-14 15:12:45 -0500

    Fix perf more

--- packages/lazy-distributor-sdk/src/functions/distributeCompressionRewards.ts
+++ packages/lazy-distributor-sdk/src/functions/distributeCompressionRewards.ts
@@ -1,17 +1,14 @@
-import { LazyDistributor } from "@helium/idls/lib/types/lazy_distributor";
-import { Asset, getAsset, getAssetProof, AssetProof } from "@helium/spl-utils";
 import { Idl, Program } from "@coral-xyz/anchor";
+import { LazyDistributor } from "@helium/idls/lib/types/lazy_distributor";
+import { Asset, AssetProof, proofArgsAndAccounts } from "@helium/spl-utils";
 import { PublicKey } from "@solana/web3.js";
-import { ConcurrentMerkleTreeAccount } from "@solana/spl-account-compression";
 
 export async function distributeCompressionRewards<IDL extends Idl>({
   program,
   assetId,
   lazyDistributor,
-  assetEndpoint,
   rewardsMint,
-  getAssetFn = getAsset,
-  getAssetProofFn = getAssetProof,
+  ...rest
 }: {
   program: Program<LazyDistributor>;
   assetId: PublicKey;
@@ -25,47 +22,30 @@ export async function distributeCompressionRewards<IDL extends Idl>({
     assetId: PublicKey
   ) => Promise<AssetProof | undefined>;
 }) {
-  // @ts-ignore
-  const endpoint = assetEndpoint || program.provider.connection._rpcEndpoint;
-  const asset = await getAssetFn(endpoint, assetId);
-  if (!asset) {
-    throw new Error("No asset with ID " + assetId.toBase58());
-  }
-  const assetProof = await getAssetProofFn(endpoint, assetId);
-  if (!assetProof) {
-    throw new Error("No asset proof with ID " + assetId.toBase58());
-  }
-  const { root, proof, leaf, treeId } = assetProof;
   const {
-    ownership: { owner },
-    compression: { leafId },
-  } = asset;
-  const canopy = await (
-    await ConcurrentMerkleTreeAccount.fromAccountAddress(program.provider.connection, treeId)
-  ).getCanopyDepth();
+    asset: {
+      ownership: { owner },
+    },
+    args,
+    accounts,
+    remainingAccounts,
+  } = await proofArgsAndAccounts({
+    connection: program.provider.connection,
+    assetId,
+    ...rest,
+  });
 
   return program.methods
     .distributeCompressionRewardsV0({
-      dataHash: asset.compression.dataHash!.toJSON().data,
-      creatorHash: asset.compression.creatorHash!.toJSON().data,
-      root: root.toBuffer().toJSON().data,
-      index: leafId!,
+      ...args,
     })
     .accounts({
+      ...accounts,
       common: {
         lazyDistributor,
         rewardsMint,
         owner,
       },
-      merkleTree: treeId,
     })
-    .remainingAccounts(
-      proof.slice(0, proof.length - canopy).map((p) => {
-        return {
-          pubkey: p,
-          isWritable: false,
-          isSigner: false,
-        };
-      })
-    );
+    .remainingAccounts(remainingAccounts);
 }
