5b78b252d4633bfa13420183dad4db2616f8774c
bry <bryanzettler@gmail.com>
2023-09-19 16:20:28 -0500

    make hnt-to-rent-service work with jupiter

--- packages/hnt-to-rent-service/src/index.ts
+++ packages/hnt-to-rent-service/src/index.ts
@@ -1,45 +1,73 @@
-import cors from "@fastify/cors";
-import {
-  PublicKey
-} from "@solana/web3.js";
-import Fastify, { FastifyInstance } from "fastify";
-import { estimate, fundFees } from "./orca";
+import cors from '@fastify/cors';
+import { PublicKey } from '@solana/web3.js';
+import Fastify, { FastifyInstance } from 'fastify';
+import * as jup from './jupiter';
+import { HNT_MINT, IOT_MINT, MOBILE_MINT } from '@helium/spl-utils';
 
-const server: FastifyInstance = Fastify({
-  logger: true,
-});
-server.register(cors, {
-  origin: "*",
-});
-server.get("/health", async () => {
-  return { ok: true };
+const server: FastifyInstance = Fastify({ logger: true });
+server.register(cors, { origin: '*' });
+
+server.get('/health', async (req, res) => {
+  res.send({ ok: true });
 });
 
 server.post<{
-  Body: { wallet: PublicKey };
-}>("/hnt-to-fees", async (request, reply) => {
-  const wallet = new PublicKey(request.body.wallet);
+  Body: { wallet: string; mint: string };
+}>('/fees', async (req, res) => {
+  try {
+    const { wallet, mint } = req.body;
+    const walletPk = new PublicKey(wallet);
+    const mintPk = new PublicKey(mint);
 
-  return Buffer.from(
-    (await fundFees(wallet)).serialize({ requireAllSignatures: false })
-  ).toJSON().data;
+    res.send(
+      Buffer.from(
+        (await jup.fundFees({ userWallet: walletPk, mint: mintPk })).serialize()
+      ).toJSON().data
+    );
+  } catch (err) {
+    res.code(500).send(err);
+    console.error(err);
+  }
 });
 
+server.post<{
+  Body: { mint: string };
+}>('/estimate', async (req, res) => {
+  try {
+    const { mint } = req.body;
+    const mintPk = new PublicKey(mint);
 
-server.get<{
-  Body: { wallet: PublicKey };
-}>("/estimate", async (request, reply) => {
-  return {
-    estimate: await estimate()
+    res.send({
+      estimate: await jup.estimate({ mint: mintPk }),
+    });
+  } catch (err) {
+    res.code(500).send(err);
+    console.error(err);
+  }
+});
+
+server.get<{}>('/estimates', async (req, res) => {
+  try {
+    res.send({
+      [HNT_MINT.toBase58()]: await jup.estimate({ mint: HNT_MINT }),
+      [MOBILE_MINT.toBase58()]: await jup.estimate({ mint: MOBILE_MINT }),
+      [IOT_MINT.toBase58()]: await jup.estimate({ mint: IOT_MINT }),
+    });
+  } catch (err) {
+    res.code(500).send(err);
+    console.error(err);
   }
 });
 
 const start = async () => {
   try {
-    await server.listen({ port: 8081, host: "0.0.0.0" });
+    await server.listen({
+      port: Number(process.env.PORT) || 8081,
+      host: '0.0.0.0',
+    });
 
     const address = server.server.address();
-    const port = typeof address === "string" ? address : address?.port;
+    const port = typeof address === 'string' ? address : address?.port;
     console.log(`Running on 0.0.0.0:${port}`);
   } catch (err) {
     server.log.error(err);
---
+++ packages/hnt-to-rent-service/src/jupiter.ts
@@ -0,0 +1,184 @@
+import { HNT_MINT, IOT_MINT, MOBILE_MINT, toBN } from '@helium/spl-utils';
+import { Configuration, DefaultApi } from '@jup-ag/api';
+import { ACCOUNT_SIZE, NATIVE_MINT, getMint } from '@solana/spl-token';
+import {
+  AddressLookupTableAccount,
+  PublicKey,
+  SystemProgram,
+  TransactionInstruction,
+  TransactionMessage,
+  VersionedTransaction,
+} from '@solana/web3.js';
+import { JUPITER_FEE_ACCOUNT, JUPITER_FEE_BPS, JUPITER_URL } from './env';
+import { provider } from './solana';
+
+export const instructionDataToTransactionInstruction = (
+  instructionPayload: any
+) => {
+  if (instructionPayload === null) {
+    return null;
+  }
+
+  return new TransactionInstruction({
+    programId: new PublicKey(instructionPayload.programId),
+    keys: instructionPayload.accounts.map((key) => ({
+      pubkey: new PublicKey(key.pubkey),
+      isSigner: key.isSigner,
+      isWritable: key.isWritable,
+    })),
+    data: Buffer.from(instructionPayload.data, 'base64'),
+  });
+};
+
+export const getAdressLookupTableAccounts = async (
+  keys: string[]
+): Promise<AddressLookupTableAccount[]> => {
+  const connection = provider.connection;
+  const addressLookupTableAccountInfos =
+    await connection.getMultipleAccountsInfo(
+      keys.map((key) => new PublicKey(key))
+    );
+
+  return addressLookupTableAccountInfos.reduce((acc, accountInfo, index) => {
+    const addressLookupTableAddress = keys[index];
+    if (accountInfo) {
+      const addressLookupTableAccount = new AddressLookupTableAccount({
+        key: new PublicKey(addressLookupTableAddress),
+        state: AddressLookupTableAccount.deserialize(accountInfo.data),
+      });
+      acc.push(addressLookupTableAccount);
+    }
+
+    return acc;
+  }, new Array<AddressLookupTableAccount>());
+};
+
+const buildJupiterApi = (() => {
+  let api: DefaultApi | undefined;
+
+  return () => {
+    if (api) return api;
+    const config = new Configuration({ basePath: JUPITER_URL });
+    api = new DefaultApi(config);
+    return api;
+  };
+})();
+
+export const estimate = async ({
+  mint,
+}: {
+  mint: PublicKey;
+}): Promise<string> => {
+  if (
+    ![
+      HNT_MINT.toBase58(),
+      IOT_MINT.toBase58(),
+      MOBILE_MINT.toBase58(),
+    ].includes(mint.toBase58())
+  ) {
+    throw new Error(
+      `Provided mint not supported by service: ${mint.toBase58()}`
+    );
+  }
+
+  const connection = provider.connection;
+  const decimals = (await getMint(connection, NATIVE_MINT)).decimals;
+  const client = buildJupiterApi();
+  const quote = await client.quoteGet({
+    amount: toBN(0.020015, decimals).toNumber(),
+    inputMint: NATIVE_MINT.toBase58(),
+    outputMint: mint.toBase58(),
+    slippageBps: 100, // 1%
+    platformFeeBps: Number(JUPITER_FEE_BPS) || 0,
+  });
+
+  if (!quote) throw new Error('Unable to quote');
+  return quote.outAmount;
+};
+
+export const fundFees = async ({
+  userWallet,
+  mint,
+}: {
+  userWallet: PublicKey;
+  mint: PublicKey;
+}): Promise<VersionedTransaction> => {
+  if (
+    ![
+      HNT_MINT.toBase58(),
+      IOT_MINT.toBase58(),
+      MOBILE_MINT.toBase58(),
+    ].includes(mint.toBase58())
+  ) {
+    throw new Error(
+      `Provided mint not supported by service: ${mint.toBase58()}`
+    );
+  }
+
+  const connection = provider.connection;
+  const platformWallet = provider.wallet;
+  const client = buildJupiterApi();
+  const inputAmount = await estimate({ mint });
+  const quote = await client.quoteGet({
+    amount: Number(inputAmount),
+    inputMint: mint.toBase58(),
+    outputMint: NATIVE_MINT.toBase58(),
+    slippageBps: 100, // 1%
+    platformFeeBps: Number(JUPITER_FEE_BPS) || 0,
+  });
+
+  // Tx contains instructions to create/close WSOL token account
+  const {
+    computeBudgetInstructions,
+    setupInstructions,
+    swapInstruction,
+    cleanupInstruction,
+    addressLookupTableAddresses,
+  } = await client.swapInstructionsPost({
+    swapRequest: {
+      quoteResponse: quote,
+      userPublicKey: userWallet.toBase58(),
+      feeAccount: JUPITER_FEE_ACCOUNT || undefined,
+    },
+  });
+
+  // create rent flash loan instructions
+  const ataRent = await connection.getMinimumBalanceForRentExemption(
+    ACCOUNT_SIZE
+  );
+
+  const borrowIx = SystemProgram.transfer({
+    fromPubkey: platformWallet.publicKey,
+    toPubkey: userWallet,
+    lamports: ataRent,
+  });
+
+  const repayIx = SystemProgram.transfer({
+    fromPubkey: userWallet,
+    toPubkey: platformWallet.publicKey,
+    lamports: ataRent,
+  });
+
+  const instructions: TransactionInstruction[] = [
+    ...computeBudgetInstructions.map(instructionDataToTransactionInstruction),
+    borrowIx,
+    ...setupInstructions.map(instructionDataToTransactionInstruction),
+    instructionDataToTransactionInstruction(swapInstruction),
+    instructionDataToTransactionInstruction(cleanupInstruction),
+    repayIx,
+  ].filter((ix) => ix !== null) as TransactionInstruction[];
+
+  const blockhash = (await connection.getLatestBlockhash()).blockhash;
+  const addressLookupTableAccounts = await getAdressLookupTableAccounts(
+    addressLookupTableAddresses
+  );
+
+  const messageV0 = new TransactionMessage({
+    payerKey: platformWallet.publicKey,
+    recentBlockhash: blockhash,
+    instructions,
+  }).compileToV0Message(addressLookupTableAccounts);
+
+  const transaction = new VersionedTransaction(messageV0);
+  return platformWallet.signTransaction(transaction);
+};
