894de7b31af528742e938591082ff2c7175245c7
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2023-09-26 12:20:42 -0500

    hotfix: Add temp endpoint to restore failed claims for unclosed epoch (#422)

--- programs/helium-sub-daos/src/instructions/clockwork/reset_dao_thread_v0.rs
+++ programs/helium-sub-daos/src/instructions/clockwork/reset_dao_thread_v0.rs
@@ -1,12 +1,8 @@
-use crate::{construct_issue_hst_kickoff_ix, current_epoch, state::*};
-use anchor_lang::{prelude::*, solana_program::native_token::LAMPORTS_PER_SOL};
+use crate::state::*;
+use anchor_lang::prelude::*;
 use anchor_spl::token::Token;
 use circuit_breaker::CircuitBreaker;
-use clockwork_sdk::{
-  cpi::{thread_create, thread_reset, thread_update},
-  state::{ThreadSettings, Trigger},
-  ThreadProgram,
-};
+use clockwork_sdk::{cpi::thread_delete, ThreadProgram};
 
 #[derive(Accounts)]
 pub struct ResetDaoThreadV0<'info> {
@@ -38,84 +34,21 @@ pub struct ResetDaoThreadV0<'info> {
 }
 
 pub fn handler(ctx: Context<ResetDaoThreadV0>) -> Result<()> {
-  let kickoff_ix = construct_issue_hst_kickoff_ix(
-    ctx.accounts.dao.key(),
-    ctx.accounts.dao.hnt_mint,
-    ctx.accounts.system_program.key(),
-    ctx.accounts.token_program.key(),
-    ctx.accounts.circuit_breaker_program.key(),
-  );
-
-  let curr_ts = Clock::get()?.unix_timestamp;
-  let epoch = current_epoch(curr_ts);
-
-  let dao_key = ctx.accounts.dao.key();
-  let dao_ei_seeds: &[&[u8]] = &[
-    "dao_epoch_info".as_bytes(),
-    dao_key.as_ref(),
-    &epoch.to_le_bytes(),
-  ];
-  let dao_epoch_info = Pubkey::find_program_address(dao_ei_seeds, &crate::id()).0;
-
   let signer_seeds: &[&[&[u8]]] = &[&[
     "dao".as_bytes(),
     ctx.accounts.dao.hnt_mint.as_ref(),
     &[ctx.accounts.dao.bump_seed],
   ]];
 
-  if ctx.accounts.thread.data_is_empty() && ctx.accounts.thread.lamports() == 0 {
-    thread_create(
-      CpiContext::new_with_signer(
-        ctx.accounts.clockwork.to_account_info(),
-        clockwork_sdk::cpi::ThreadCreate {
-          authority: ctx.accounts.dao.to_account_info(),
-          payer: ctx.accounts.thread_payer.to_account_info(),
-          thread: ctx.accounts.thread.to_account_info(),
-          system_program: ctx.accounts.system_program.to_account_info(),
-        },
-        signer_seeds,
-      ),
-      LAMPORTS_PER_SOL,
-      "issue_hst".to_string().as_bytes().to_vec(),
-      vec![kickoff_ix.into()],
-      Trigger::Account {
-        address: dao_epoch_info,
-        offset: 8,
-        size: 1,
-      },
-    )?;
-  } else {
-    thread_reset(CpiContext::new_with_signer(
-      ctx.accounts.clockwork.to_account_info(),
-      clockwork_sdk::cpi::ThreadReset {
-        authority: ctx.accounts.dao.to_account_info(),
-        thread: ctx.accounts.thread.to_account_info(),
-      },
-      signer_seeds,
-    ))?;
-    thread_update(
-      CpiContext::new_with_signer(
-        ctx.accounts.clockwork.to_account_info(),
-        clockwork_sdk::cpi::ThreadUpdate {
-          authority: ctx.accounts.dao.to_account_info(),
-          thread: ctx.accounts.thread.to_account_info(),
-          system_program: ctx.accounts.system_program.to_account_info(),
-        },
-        signer_seeds,
-      ),
-      ThreadSettings {
-        name: None,
-        fee: None,
-        instructions: Some(vec![kickoff_ix.into()]),
-        rate_limit: None,
-        trigger: Some(Trigger::Account {
-          address: dao_epoch_info,
-          offset: 8,
-          size: 1,
-        }),
-      },
-    )?;
-  }
+  thread_delete(CpiContext::new_with_signer(
+    ctx.accounts.clockwork.to_account_info(),
+    clockwork_sdk::cpi::ThreadDelete {
+      authority: ctx.accounts.dao.to_account_info(),
+      thread: ctx.accounts.thread.to_account_info(),
+      close_to: ctx.accounts.authority.to_account_info(),
+    },
+    signer_seeds,
+  ))?;
 
   Ok(())
 }
