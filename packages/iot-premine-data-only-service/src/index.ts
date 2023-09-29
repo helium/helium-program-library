1ca9cfc0c5e4fd89cefb7a9b05944102b3b7a9b1
Chewing Glass <chewinglass123@gmail.com>
2023-09-29 13:23:50 -0500

    Fix build

--- packages/iot-premine-data-only-service/src/index.ts
+++ packages/iot-premine-data-only-service/src/index.ts
@@ -114,6 +114,7 @@ import { decompress, decompressSigners } from "./utils";
                   lazySigner,
                   block,
                   systemProgram: SystemProgram.programId,
+                  executedTransactions: executedTransactionsKey,
                 })
                 .remainingAccounts([
                   ...compiledTx.accounts,
