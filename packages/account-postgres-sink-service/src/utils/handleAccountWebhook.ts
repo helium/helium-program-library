fea7ec9b3e958dbab744c32ab9d44d7ddc91a83d
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2023-10-06 10:36:26 -0400

    Fix coordinates and switch to kamoot (#440)
    * Fix coordinates and switch to kamoot
    
    * Add limit to webhook
    
    * Make photon url configurable
    
    * Rm lat lng, street

--- packages/account-postgres-sink-service/src/utils/handleAccountWebhook.ts
+++ packages/account-postgres-sink-service/src/utils/handleAccountWebhook.ts
@@ -7,8 +7,7 @@ import { sanitizeAccount } from "./sanitizeAccount";
 import cachedIdlFetch from "./cachedIdlFetch";
 import { FastifyInstance } from "fastify";
 import { IAccountConfig, IInitedPlugin } from "../types";
-import { initPlugins } from "../plugins";
-import { truthy } from "./upsertProgramAccounts";
+import pLimit from "p-limit";
 
 interface HandleAccountWebhookArgs {
   fastify: FastifyInstance;
@@ -16,77 +15,83 @@ interface HandleAccountWebhookArgs {
   accounts: IAccountConfig[];
   account: any;
   sequelize?: Sequelize;
-  pluginsByAccountType: Record<string, IInitedPlugin[]>
+  pluginsByAccountType: Record<string, IInitedPlugin[]>;
 }
 
-export async function handleAccountWebhook({
+// Ensure we never have more txns open than the pool size - 1
+const limit = pLimit(
+  (process.env.PG_POOL_SIZE ? Number(process.env.PG_POOL_SIZE) : 5) - 1
+);
+export function handleAccountWebhook({
   fastify,
   programId,
   accounts,
   account,
   sequelize = database,
-  pluginsByAccountType
+  pluginsByAccountType,
 }: HandleAccountWebhookArgs) {
-  const idl = await cachedIdlFetch.fetchIdl({
-    programId: programId.toBase58(),
-    provider,
-  });
+  return limit(async () => {
+    const idl = await cachedIdlFetch.fetchIdl({
+      programId: programId.toBase58(),
+      provider,
+    });
 
-  if (!idl) {
-    throw new Error(`unable to fetch idl for ${programId}`);
-  }
-
-  if (
-    !accounts.every(({ type }) =>
-      idl.accounts!.some(({ name }) => name === type)
-    )
-  ) {
-    throw new Error("idl does not have every account type");
-  }
-
-  const t = await sequelize.transaction();
-  const now = new Date().toISOString();
-  try {
-    const program = new anchor.Program(idl, programId, provider);
-    const data = Buffer.from(account.data[0], account.data[1]);
-
-    const accName = accounts.find(({ type }) => {
-      return (
-        data &&
-        anchor.BorshAccountsCoder.accountDiscriminator(type).equals(
-          data.subarray(0, 8)
-        )
-      );
-    })?.type;
-
-    if (accName) {
-      const decodedAcc = program.coder.accounts.decode(
-        accName!,
-        data as Buffer
-      );
-      let sanitized = sanitizeAccount(decodedAcc);
-      for (const plugin of pluginsByAccountType[accName]) {
-        if (plugin?.processAccount) {
-          sanitized = await plugin.processAccount(sanitized, t);
-        }
-      }
-      const model = sequelize.models[accName];
-      await model.upsert(
-        {
-          address: account.pubkey,
-          refreshed_at: now,
-          ...sanitized,
-        },
-        { transaction: t }
-      );
+    if (!idl) {
+      throw new Error(`unable to fetch idl for ${programId}`);
     }
 
-    await t.commit();
-    // @ts-ignore
-    fastify.customMetrics.accountWebhookCounter.inc();
-  } catch (err) {
-    await t.rollback();
-    console.error("While inserting, err", err);
-    throw err;
-  }
+    if (
+      !accounts.every(({ type }) =>
+        idl.accounts!.some(({ name }) => name === type)
+      )
+    ) {
+      throw new Error("idl does not have every account type");
+    }
+
+    const t = await sequelize.transaction();
+    const now = new Date().toISOString();
+    try {
+      const program = new anchor.Program(idl, programId, provider);
+      const data = Buffer.from(account.data[0], account.data[1]);
+
+      const accName = accounts.find(({ type }) => {
+        return (
+          data &&
+          anchor.BorshAccountsCoder.accountDiscriminator(type).equals(
+            data.subarray(0, 8)
+          )
+        );
+      })?.type;
+
+      if (accName) {
+        const decodedAcc = program.coder.accounts.decode(
+          accName!,
+          data as Buffer
+        );
+        let sanitized = sanitizeAccount(decodedAcc);
+        for (const plugin of pluginsByAccountType[accName]) {
+          if (plugin?.processAccount) {
+            sanitized = await plugin.processAccount(sanitized, t);
+          }
+        }
+        const model = sequelize.models[accName];
+        await model.upsert(
+          {
+            address: account.pubkey,
+            refreshed_at: now,
+            ...sanitized,
+          },
+          { transaction: t }
+        );
+      }
+
+      await t.commit();
+      // @ts-ignore
+      fastify.customMetrics.accountWebhookCounter.inc();
+    } catch (err) {
+      await t.rollback();
+      console.error("While inserting, err", err);
+      throw err;
+    }
+  });
 }
--- packages/account-postgres-sink-service/src/utils/mapboxService.ts
+++ packages/account-postgres-sink-service/src/utils/mapboxService.ts
@@ -33,10 +33,52 @@ export class MapboxService {
 
   public async fetchLocation(coords: [number, number]): Promise<any> {
     return this.limit(async () => {
-      const response = await this.axiosInstance.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords[0]},${coords[1]}.json?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`
+      const response = await axios.get(
+        `${process.env.PHOTON_URL!}/reverse?lat=${coords[0]}&lon=${coords[1]}`
       );
+
+      // Uncomment for mapbox impl
+      // const response = await this.axiosInstance.get(
+      //   `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords[0]},${coords[1]}.json?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`
+      // );
       return response.data;
     });
+  }
+
+  public async fetchParsedLocation(
+    coords: [number, number]
+  ): Promise<{
+    city?: string;
+    state?: string;
+    country?: string;
+    name?: string;
+    raw: any[];
+  }> {
+    const response = await this.fetchLocation(coords);
+    let city, state, country, name;
+    if (response.features && response.features.length > 0) {
+      ({
+        properties: { city, state, country, name },
+      } = response.features[0]);
+    }
+
+    // Uncomment for mapbox impl
+    // let placeName, parts, street, city, state, country;
+    // if (response.features && response.features.length > 0) {
+    //   placeName = response.features[0].place_name;
+    //   parts = placeName.split(",");
+    //   street = parts[parts.length - 4]?.trim();
+    //   city = parts[parts.length - 3]?.trim();
+    //   state = parts[parts.length - 2]?.split(" ")[1]?.trim();
+    //   country = parts[parts.length - 1]?.trim();
+    // }
+
+    return {
+      city,
+      state,
+      country,
+      name,
+      raw: response.features,
+    };
   }
 }
