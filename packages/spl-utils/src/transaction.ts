faa82384c803208958c3eba9dc4b72953d06ba4f
Luis Perrone <peronif5@gmail.com>
2024-02-04 20:01:22 -0600

    Add send with squads to utils

--- packages/spl-utils/src/transaction.ts
+++ packages/spl-utils/src/transaction.ts
@@ -18,7 +18,9 @@ import {
 } from "@solana/web3.js";
 import bs58 from "bs58";
 import { ProgramError } from "./anchorError";
-import { estimatePrioritizationFee } from "./priorityFees";
+import { estimatePrioritizationFee, withPriorityFees } from "./priorityFees";
+import Squads, { getTxPDA } from "@sqds/sdk";
+import { BN } from "bn.js";
 
 export const chunks = <T>(array: T[], size: number): T[][] =>
   Array.apply(0, new Array(Math.ceil(array.length / size))).map((_, index) =>
@@ -883,5 +885,140 @@ export async function batchParallelInstructionsWithPriorityFee(
     triesRemaining,
     extraSigners,
     maxSignatureBatch
+  );
+}
+
+export async function sendInstructionsOrSquads({
+  provider,
+  instructions,
+  signers = [],
+  payer = provider.wallet.publicKey,
+  commitment = "confirmed",
+  idlErrors = new Map(),
+  executeTransaction = false,
+  squads,
+  multisig,
+  authorityIndex,
+}: {
+  executeTransaction?: boolean; // Will execute the transaction immediately. Only works if the squads multisig is only 1 wallet threshold or signers is complete
+  provider: AnchorProvider;
+  instructions: TransactionInstruction[];
+  signers?: Signer[];
+  payer?: PublicKey;
+  commitment?: Commitment;
+  idlErrors?: Map<number, string>;
+  squads: Squads;
+  multisig?: PublicKey;
+  authorityIndex?: number;
+}): Promise<string | undefined> {
+  if (!multisig) {
+    return await sendInstructions(
+      provider,
+      await withPriorityFees({
+        connection: provider.connection,
+        computeUnits: 1000000,
+        instructions,
+      }),
+      signers,
+      payer,
+      commitment,
+      idlErrors
+    );
+  }
+
+  const signerSet = new Set(
+    instructions
+      .map((ix) =>
+        ix.keys.filter((k) => k.isSigner).map((k) => k.pubkey.toBase58())
+      )
+      .flat()
+  );
+  const signerKeys = Array.from(signerSet).map((k) => new PublicKey(k));
+
+  const nonMissingSignerIxs = instructions.filter(
+    (ix) =>
+      !ix.keys.some(
+        (k) => k.isSigner && !k.pubkey.equals(provider.wallet.publicKey)
+      )
+  );
+  const squadsSignatures = signerKeys.filter(
+    (k) =>
+      !k.equals(provider.wallet.publicKey) &&
+      !signers.some((s) => s.publicKey.equals(k))
+  );
+
+  if (squadsSignatures.length == 0) {
+    return await sendInstructions(
+      provider,
+      await withPriorityFees({
+        connection: provider.connection,
+        computeUnits: 1000000,
+        instructions: nonMissingSignerIxs,
+      }),
+      signers,
+      payer,
+      commitment,
+      idlErrors
+    );
+  }
+
+  if (squadsSignatures.length >= 2) {
+    throw new Error("Too many missing signatures");
+  }
+
+  const txIndex = await squads.getNextTransactionIndex(multisig);
+  const ix = await squads.buildCreateTransaction(
+    multisig,
+    authorityIndex!,
+    txIndex
+  );
+  await sendInstructions(
+    provider,
+    await withPriorityFees({
+      connection: provider.connection,
+      instructions: [ix],
+      computeUnits: 200000,
+    })
+  );
+  const [txKey] = await getTxPDA(
+    multisig,
+    new BN(txIndex),
+    squads.multisigProgramId
+  );
+  let index = 1;
+  for (const ix of instructions.filter(
+    (ix) => !ix.programId.equals(ComputeBudgetProgram.programId)
+  )) {
+    await sendInstructions(
+      provider,
+      await withPriorityFees({
+        connection: provider.connection,
+        instructions: [
+          await squads.buildAddInstruction(multisig, txKey, ix, index),
+        ],
+        computeUnits: 200000,
+      })
+    );
+    index++;
+  }
+
+  const ixs: TransactionInstruction[] = [];
+  ixs.push(await squads.buildActivateTransaction(multisig, txKey));
+  ixs.push(await squads.buildApproveTransaction(multisig, txKey));
+
+  if (executeTransaction) {
+    ixs.push(
+      await squads.buildExecuteTransaction(txKey, provider.wallet.publicKey)
+    );
+  }
+
+  await sendInstructions(
+    provider,
+    await withPriorityFees({
+      connection: provider.connection,
+      computeUnits: 1000000,
+      instructions: ixs,
+    }),
+    signers
   );
 }
