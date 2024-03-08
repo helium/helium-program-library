0e61e2b5d794616d5688db573bce4e63e12fc18d
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2024-03-08 14:12:12 -0800

    Use helius priority fees (#603)

--- packages/spl-utils/src/priorityFees.ts
+++ packages/spl-utils/src/priorityFees.ts
@@ -22,55 +22,68 @@ export async function estimatePrioritizationFee(
   ixs: TransactionInstruction[],
   basePriorityFee?: number
 ): Promise<number> {
-  const writableAccounts = ixs
-    .map((x) => x.keys.filter((a) => a.isWritable).map((k) => k.pubkey))
-    .flat();
-  const uniqueWritableAccounts = [
-    ...new Set(writableAccounts.map((x) => x.toBase58())),
-  ]
+  const accounts = ixs.map((x) => x.keys.map((k) => k.pubkey)).flat();
+  const uniqueAccounts = [...new Set(accounts.map((x) => x.toBase58()))]
     .map((a) => new PublicKey(a))
     .slice(0, MAX_RECENT_PRIORITY_FEE_ACCOUNTS);
 
-  const priorityFees = await connection.getRecentPrioritizationFees({
-    lockedWritableAccounts: uniqueWritableAccounts,
-  });
-
-  if (priorityFees.length < 1) {
-    return Math.max(basePriorityFee || 0, 1);
-  }
-
-  // get max priority fee per slot (and sort by slot from old to new)
-  const groupedBySlot = priorityFees.reduce((acc, fee) => {
-    const key = fee.slot;
-    if (!acc[key]) {
-      acc[key] = [];
-    }
-    acc[key].push(fee);
-    return acc;
-  }, {} as Record<string, RecentPrioritizationFees[]>);
-
-  const maxFeeBySlot = Object.keys(groupedBySlot).reduce((acc, slot) => {
-    acc[slot] = groupedBySlot[slot].reduce((max, fee) => {
-      return fee.prioritizationFee > max.prioritizationFee ? fee : max;
+  try {
+    // @ts-ignore
+    const { result: { priorityFeeEstimate } } = await connection._rpcRequest(
+      "getPriorityFeeEstimate",
+      connection._buildArgs([
+        {
+          accountKeys: uniqueAccounts.map((a) => a.toBase58()),
+        },
+      ])
+    );
+    return Math.max(basePriorityFee || 1, Math.ceil(priorityFeeEstimate));
+  } catch (e: any) {
+    console.error(
+      "Failed to use getPriorityFeeEstimate, falling back to getRecentPrioritizationFees",
+      e
+    );
+    const priorityFees = await connection.getRecentPrioritizationFees({
+      lockedWritableAccounts: uniqueAccounts,
     });
-    return acc;
-  }, {} as Record<string, RecentPrioritizationFees>);
-  const maximumFees = Object.values(maxFeeBySlot).sort(
-    (a: RecentPrioritizationFees, b: RecentPrioritizationFees) =>
-      a.slot - b.slot
-  ) as RecentPrioritizationFees[];
 
-  // get median of last 20 fees
-  const recentFees = maximumFees.slice(Math.max(maximumFees.length - 20, 0));
-  const mid = Math.floor(recentFees.length / 2);
-  const medianFee =
-    recentFees.length % 2 !== 0
-      ? recentFees[mid].prioritizationFee
-      : (recentFees[mid - 1].prioritizationFee +
-          recentFees[mid].prioritizationFee) /
-        2;
+    if (priorityFees.length < 1) {
+      return Math.max(basePriorityFee || 0, 1);
+    }
 
-  return Math.max(basePriorityFee || 1, Math.ceil(medianFee));
+    // get max priority fee per slot (and sort by slot from old to new)
+    const groupedBySlot = priorityFees.reduce((acc, fee) => {
+      const key = fee.slot;
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(fee);
+      return acc;
+    }, {} as Record<string, RecentPrioritizationFees[]>);
+
+    const maxFeeBySlot = Object.keys(groupedBySlot).reduce((acc, slot) => {
+      acc[slot] = groupedBySlot[slot].reduce((max, fee) => {
+        return fee.prioritizationFee > max.prioritizationFee ? fee : max;
+      });
+      return acc;
+    }, {} as Record<string, RecentPrioritizationFees>);
+    const maximumFees = Object.values(maxFeeBySlot).sort(
+      (a: RecentPrioritizationFees, b: RecentPrioritizationFees) =>
+        a.slot - b.slot
+    ) as RecentPrioritizationFees[];
+
+    // get median of last 20 fees
+    const recentFees = maximumFees.slice(Math.max(maximumFees.length - 20, 0));
+    const mid = Math.floor(recentFees.length / 2);
+    const medianFee =
+      recentFees.length % 2 !== 0
+        ? recentFees[mid].prioritizationFee
+        : (recentFees[mid - 1].prioritizationFee +
+            recentFees[mid].prioritizationFee) /
+          2;
+
+    return Math.max(basePriorityFee || 1, Math.ceil(medianFee));
+  }
 }
 
 export async function withPriorityFees({
