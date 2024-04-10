c6235d8224d88f6b4cd804efafcb01c749d68c66
Noah Prince <chewinglass123@gmail.com>
2024-04-10 06:19:58 -0500

    Add forceRequery option

--- packages/distributor-oracle/src/client.ts
+++ packages/distributor-oracle/src/client.ts
@@ -105,7 +105,8 @@ export async function getPendingRewards(
   lazyDistributor: PublicKey,
   dao: PublicKey,
   entityKeys: string[],
-  encoding: BufferEncoding | "b58" = "b58"
+  encoding: BufferEncoding | "b58" = "b58",
+  forceRequery = false
 ): Promise<Record<string, string>> {
   const oracleRewards = await getBulkRewards(
     program,
@@ -129,7 +130,7 @@ export async function getPendingRewards(
       >("KeyToAssetV0", account.data),
     }),
     true,
-    false
+    forceRequery
   );
   keyToAssets.forEach((kta, index) => {
     if (!kta?.info) {
@@ -149,7 +150,9 @@ export async function getPendingRewards(
       info: program.coder.accounts.decode<
         IdlAccounts<LazyDistributor>["recipientV0"]
       >("RecipientV0", account.data),
-    })
+    }),
+    false,
+    forceRequery
   );
   const withRecipients = recipients.map((recipient, index) => {
     return {
