dbd12cc36141beb84bc5a64f6fca827090b19307
Bryan <bryanzettler@gmail.com>
2024-05-08 14:04:03 -0500

    Feat/rpc max retries 0 (#647)
    * Add maxRetries: 0 to sendTransaction calls
    
    * add param and default

--- packages/spl-utils/src/transaction.ts
+++ packages/spl-utils/src/transaction.ts
@@ -171,6 +171,7 @@ export async function sendInstructions(
       tx.serialize(),
       {
         skipPreflight: true,
+        maxRetries: 0,
       },
       commitment
     );
@@ -569,7 +570,8 @@ export async function bulkSendRawTransactions(
   txs: Buffer[],
   onProgress?: (status: Status) => void,
   lastValidBlockHeight?: number,
-  skipPreflight: boolean = true
+  skipPreflight: boolean = true,
+  maxRetries: number = 0
 ): Promise<string[]> {
   const txBatchSize = TX_BATCH_SIZE;
   let totalProgress = 0;
@@ -603,6 +605,7 @@ export async function bulkSendRawTransactions(
         for (const tx of chunk) {
           const txid = await connection.sendRawTransaction(tx, {
             skipPreflight,
+            maxRetries,
           });
           txids.push(txid);
         }
