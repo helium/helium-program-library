1237b3967491a406b4b16f1ad44acf681fe01d9d
Noah Prince <chewinglass123@gmail.com>
2024-06-10 10:45:33 -0700

    Reset token consts

--- packages/spl-utils/src/constants.ts
+++ packages/spl-utils/src/constants.ts
@@ -4,22 +4,13 @@ export const DC_MINT = new PublicKey(
   "dcuc8Amr83Wz27ZkQ2K9NS6r8zRpf1J6cvArEBDZDmm"
 );
 
-// export const HNT_MINT = new PublicKey("hntyVP6YFm1Hg25TN9WGLqM12b8TQmcknKrdu1oxWux");
-export const HNT_MINT = new PublicKey(
-  "APqAVo5q9erS8GaXcbJuy3Gx4ikuSzXjzY4SnyppPUm1"
-);
+export const HNT_MINT = new PublicKey("hntyVP6YFm1Hg25TN9WGLqM12b8TQmcknKrdu1oxWux");
 
-// export const MOBILE_MINT = new PublicKey(
-//   "mb1eu7TzEc71KxDpsmsKoucSSuuoGLv1drys1oP2jh6"
-// );
 export const MOBILE_MINT = new PublicKey(
-  "CoQciaEADT77zudZQm4atzWjHmHcJKfzZBivbifHScvZ"
+  "mb1eu7TzEc71KxDpsmsKoucSSuuoGLv1drys1oP2jh6"
 );
 
-// export const IOT_MINT = new PublicKey("iotEVVZLEywoTn1QdwNPddxPWszn3zFhEot3MfL9fns");
-export const IOT_MINT = new PublicKey(
-  "g69eEQjY4bv1UvjaeTUcaCNoJ6v8jSsjL3dojN6uC4B"
-);
+export const IOT_MINT = new PublicKey("iotEVVZLEywoTn1QdwNPddxPWszn3zFhEot3MfL9fns");
 
 // TODO: Replace with actual HNT feed
 export const HNT_PYTH_PRICE_FEED = new PublicKey(
