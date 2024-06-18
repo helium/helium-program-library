90117771a44d8738da9fd9ce76dbf428e9befe6a
Noah Prince <chewinglass123@gmail.com>
2024-06-18 13:15:43 -0700

    Fix bug with oracle price getter

--- packages/currency-utils/src/currencyUtils.ts
+++ packages/currency-utils/src/currencyUtils.ts
@@ -4,7 +4,7 @@ import {
   PythSolanaReceiverProgram,
   pythSolanaReceiverIdl,
 } from "@pythnetwork/pyth-solana-receiver";
-import { Program } from "@coral-xyz/anchor";
+import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
 import { token } from "@coral-xyz/anchor/dist/cjs/utils";
 
 export const getBalance = async ({
@@ -37,12 +37,15 @@ export const getOraclePrice = async ({
 }) => {
   const pythProgram: Program<PythSolanaReceiverProgram> = new Program(
     pythSolanaReceiverIdl,
-    new PublicKey("rec5EKMGg6MxZYaMdyBfgwp4d5rB9T1VQH5pJv5LtFJ")
+    new PublicKey("rec5EKMGg6MxZYaMdyBfgwp4d5rB9T1VQH5pJv5LtFJ"),
+    new AnchorProvider(connection, {} as Wallet, {
+      skipPreflight: true,
+    })
   );
 
   const data = await pythProgram.account.priceUpdateV2.fetch(
     new PublicKey("DQ4C1tzvu28cwo1roN1Wm6TW35sfJEjLh517k3ZeWevx")
   );
 
-  return data
+  return data;
 };
