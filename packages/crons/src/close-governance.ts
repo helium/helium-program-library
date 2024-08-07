6fecd5468f65b3f84652fb851ff21de96412ea3c
bry <bryanzettler@gmail.com>
2024-08-07 12:59:41 -0500

    import fn from utils

--- packages/crons/src/close-governance.ts
+++ packages/crons/src/close-governance.ts
@@ -21,6 +21,7 @@ import {
   TransactionInstruction,
 } from "@solana/web3.js";
 import pLimit from "p-limit";
+import { flattenAndLimitSubArrays } from "./utils";
 
 async function getSolanaUnixTimestamp(
   provider: anchor.AnchorProvider
@@ -29,43 +30,6 @@ async function getSolanaUnixTimestamp(
   const unixTime = clock!.data.readBigInt64LE(8 * 4);
   return unixTime;
 }
-
-const flattenAndLimitSubArrays = <T>(
-  multiDimArray: T[][],
-  maxItems: number
-): T[][] => {
-  const result: T[][] = [];
-  let currentBucket: T[] = [];
-
-  for (const subArray of multiDimArray) {
-    // If the current sub-array itself is larger than maxItems, we need to split it
-    if (subArray.length > maxItems) {
-      if (currentBucket.length > 0) {
-        result.push(currentBucket);
-        currentBucket = [];
-      }
-      for (let i = 0; i < subArray.length; i += maxItems) {
-        result.push(subArray.slice(i, i + maxItems));
-      }
-      continue;
-    }
-
-    // Check if adding the current sub-array would exceed maxItems in the current bucket
-    if (currentBucket.length + subArray.length > maxItems) {
-      result.push(currentBucket);
-      currentBucket = [];
-    }
-
-    currentBucket.push(...subArray);
-  }
-
-  // Add the last bucket if it has any items
-  if (currentBucket.length > 0) {
-    result.push(currentBucket);
-  }
-
-  return result;
-};
 
 (async () => {
   try {
