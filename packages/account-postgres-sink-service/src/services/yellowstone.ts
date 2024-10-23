c7e6b07677a602657c1d03922bccfab5405b47cb
bry <bryanzettler@gmail.com>
2024-10-23 16:43:43 -0500

    dont make HELIUS auth a required env

--- packages/account-postgres-sink-service/src/services/yellowstone.ts
+++ packages/account-postgres-sink-service/src/services/yellowstone.ts
@@ -20,6 +20,10 @@ export const setupYellowstone = async (
   server: FastifyInstance,
   configs: IConfig[]
 ) => {
+  if (!YELLOWSTONE_TOKEN) {
+    throw new Error("Yellowstone token not available");
+  }
+
   let isReconnecting = false;
   const pluginsByAccountTypeByProgram = await getPluginsByAccountTypeByProgram(
     configs
