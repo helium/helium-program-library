b84d80c036be47656502377a0863928930ec7a31
bry <bryanzettler@gmail.com>
2024-11-20 12:24:33 -0600

    revert constants

--- packages/spl-utils/src/constants.ts
+++ packages/spl-utils/src/constants.ts
@@ -1,20 +1,16 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const DC_MINT = new PublicKey(
-  "EMsZWzk2zqwtTvvimTVD1qq4usxsNptt1HWSHYurcBWA"
+  "dcuc8Amr83Wz27ZkQ2K9NS6r8zRpf1J6cvArEBDZDmm"
 );
 
-export const HNT_MINT = new PublicKey(
-  "2ZiWvqkZ8DCZwjoJ3HudxnChPrPPk6UVWraouC3GHvRN"
-);
+export const HNT_MINT = new PublicKey("hntyVP6YFm1Hg25TN9WGLqM12b8TQmcknKrdu1oxWux");
 
 export const MOBILE_MINT = new PublicKey(
-  "BuPZsYRpvVrYUPZMqZAJun2E7mTeAUwpCekb2uJUvapr"
+  "mb1eu7TzEc71KxDpsmsKoucSSuuoGLv1drys1oP2jh6"
 );
 
-export const IOT_MINT = new PublicKey(
-  "2GtqVJo9yKXuJbyZrxr3ouQbU8AXDJQq74DQpFXFrHg7"
-);
+export const IOT_MINT = new PublicKey("iotEVVZLEywoTn1QdwNPddxPWszn3zFhEot3MfL9fns");
 
 // TODO: Replace with actual HNT feed
 export const HNT_PYTH_PRICE_FEED = new PublicKey(
@@ -29,13 +25,6 @@ export const HELIUM_COMMON_LUT = new PublicKey(
   "43eY9L2spbM2b1MPDFFBStUiFGt29ziZ1nc1xbpzsfVt"
 );
 
-export const MOBILE_PRICE_FEED = new PublicKey(
-  "DQ4C1tzvu28cwo1roN1Wm6TW35sfJEjLh517k3ZeWevx"
-);
-
-export const IOT_PRICE_FEED = new PublicKey(
-  "8UYEn5Weq7toHwgcmctvcAxaNJo3SJxXEayM57rpoXr9"
-);
 export type Network = "hnt" | "mobile" | "iot";
 export const networksToMint: { [Network: string]: PublicKey } = {
   hnt: HNT_MINT,
