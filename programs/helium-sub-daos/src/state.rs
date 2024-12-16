72ebeaa274983a28a560a85763abd6fc51f03658
Noah Prince <chewinglass123@gmail.com>
2024-12-16 10:51:20 -0300

    Fix space

--- programs/helium-sub-daos/src/state.rs
+++ programs/helium-sub-daos/src/state.rs
@@ -282,7 +282,7 @@ pub struct SubDaoEpochInfoV0 {
 }
 
 impl SubDaoEpochInfoV0 {
-  pub const SIZE: usize = 60 + 8 + std::mem::size_of::<SubDaoEpochInfoV0>() - 8 - 8; // subtract 8 the extra u64 we added to vehnt, and dc onboarding fees paid
+  pub const SIZE: usize = 60 + 8 + std::mem::size_of::<SubDaoEpochInfoV0>() - 8 - 8 - 8; // subtract 8 the extra u64 we added to vehnt, dc onboarding fees paid, and hnt rewards issued
 }
 impl SubDaoEpochInfoV0 {
   pub fn start_ts(&self) -> i64 {
