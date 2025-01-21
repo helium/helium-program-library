b5e682aca15a9984dc308d47eabd1db342aa1494
Noah Prince <chewinglass123@gmail.com>
2025-01-21 10:24:37 -0800

    WIP

--- anchor-30-programs/programs/hpl-crons/src/lib.rs
+++ anchor-30-programs/programs/hpl-crons/src/lib.rs
@@ -20,8 +20,10 @@ mod helium_sub_daos {
   declare_program!(helium_sub_daos);
 }
 
+const EPOCH_LENGTH: u64 = 60 * 60 * 24;
+
 pub fn current_epoch(unix_timestamp: i64) -> u64 {
-  (unix_timestamp / (EPOCH_LENGTH)).try_into().unwrap()
+  (unix_timestamp / (EPOCH_LENGTH as i64)).try_into().unwrap()
 }
 
 #[program]
@@ -35,21 +37,20 @@ pub mod hpl_crons {
   use helium_sub_daos::helium_sub_daos::types::CalculateUtilityScoreArgsV0;
   use tuktuk_program::{
     compile_transaction,
-    tuktuk::{
-      cpi::{accounts::QueueTaskV0, queue_task_v0},
-      types::TriggerV0,
-    },
-    types::QueueTaskArgsV0,
+    tuktuk::types::TriggerV0,
     write_return_tasks::{write_return_tasks, AccountWithSeeds, PayerInfo, WriteReturnTasksArgs},
     RunTaskReturnV0, TaskReturnV0, TransactionSourceV0,
   };
 
   use super::*;
 
-  pub fn queue_daily_tasks(ctx: Context<QueueDailyTasks>) -> Result<()> {
+  pub fn queue_end_epoch(ctx: Context<QueueEndEpoch>) -> Result<RunTaskReturnV0> {
     let circuit_breaker_program =
       Pubkey::from_str("circAbx64bbsscPbQzZAUvuXpHqrCe6fLMzc2uKXz9g").unwrap();
-    let sub_daos = vec![ctx.accounts.iot_sub_dao, ctx.accounts.mobile_sub_dao];
+    let sub_daos = vec![
+      ctx.accounts.iot_sub_dao.clone(),
+      ctx.accounts.mobile_sub_dao.clone(),
+    ];
     // This should be running an hour before the epoch ends, so what is now the current epoch is actually the previous epoch
     let prev_epoch = current_epoch(Clock::get().unwrap().unix_timestamp);
     let curr_epoch = prev_epoch + 1;
@@ -150,27 +151,57 @@ pub mod hpl_crons {
       };
       (calculate, issue)
     });
-    let (compiled_tx, _) = compile_transaction(
-      vec![Instruction {
-        program_id: crate::ID,
-        accounts: crate::__cpi_client_accounts_recurring_task::RecurringTask {
-          system_program: ctx.accounts.system_program.to_account_info(),
-        }
-        .to_account_metas(None)
-        .to_vec(),
-        data: crate::instruction::RecurringTask.data(),
-      }],
-      vec![],
-    )
-    .unwrap();
+    let no_emit_wallet = Pubkey::find_program_address(&[b"not_emitted_counter"], &no_emit::ID).0;
+    let no_emit = Instruction {
+      program_id: no_emit::ID,
+      accounts: no_emit::no_emit::client::accounts::NoEmitV0 {
+        system_program: ctx.accounts.system_program.key(),
+        payer: ctx.accounts.payer.key(),
+        no_emit_wallet,
+        not_emitted_counter: Pubkey::find_program_address(
+          &[b"not_emitted_counter", ctx.accounts.dao.hnt_mint.as_ref()],
+          &no_emit::ID,
+        )
+        .0,
+        token_account: spl_associated_token_account::get_associated_token_address(
+          &no_emit_wallet,
+          &ctx.accounts.dao.hnt_mint,
+        ),
+        mint: ctx.accounts.dao.hnt_mint,
+        token_program: spl_token::ID,
+      }
+      .to_account_metas(None)
+      .to_vec(),
+      data: no_emit::no_emit::client::args::NoEmitV0.data(),
+    };
+    let (calculates, issues): (Vec<_>, Vec<_>) = sub_dao_ixs.unzip();
+    let mut ixs = Vec::new();
+    ixs.extend(calculates);
+    ixs.extend(issues);
+    ixs.push(no_emit);
+    let (compiled_tx, _) = compile_transaction(ixs, vec![vec![b"helium".to_vec()]]).unwrap();
 
-    msg!("Rescheduling task via return account");
+    let reschedule_ix = Instruction {
+      program_id: crate::ID,
+      accounts: crate::__cpi_client_accounts_queue_end_epoch::QueueEndEpoch {
+        system_program: ctx.accounts.system_program.to_account_info(),
+        payer: ctx.accounts.payer.to_account_info(),
+        dao: ctx.accounts.dao.to_account_info(),
+        iot_sub_dao: ctx.accounts.iot_sub_dao.to_account_info(),
+        mobile_sub_dao: ctx.accounts.mobile_sub_dao.to_account_info(),
+        task_return_account: ctx.accounts.task_return_account.to_account_info(),
+        task_queue: ctx.accounts.task_queue.to_account_info(),
+      }
+      .to_account_metas(None)
+      .to_vec(),
+      data: crate::instruction::QueueEndEpoch.data(),
+    };
+    let (compiled_reschedule_tx, _) =
+      compile_transaction(vec![reschedule_ix], vec![vec![b"helium".to_vec()]]).unwrap();
+
     let return_accounts = write_return_tasks(WriteReturnTasksArgs {
       program_id: crate::ID,
-      payer_info: PayerInfo::SystemPayer {
-        account_info: ctx.accounts.queue_authority.to_account_info(),
-        seeds: vec![b"queue_authority".to_vec(), vec![ctx.bumps.queue_authority]],
-      },
+      payer_info: PayerInfo::Signer(ctx.accounts.payer.to_account_info()),
       accounts: vec![AccountWithSeeds {
         account: ctx.accounts.task_return_account.to_account_info(),
         seeds: vec![
@@ -179,12 +210,26 @@ pub mod hpl_crons {
         ],
       }],
       system_program: ctx.accounts.system_program.to_account_info(),
-      tasks: (0..15).map(|_| TaskReturnV0 {
-        trigger: TriggerV0::Now,
-        transaction: TransactionSourceV0::CompiledV0(compiled_tx.clone()),
-        crank_reward: None,
-        free_tasks: 1,
-      }),
+      tasks: vec![
+        TaskReturnV0 {
+          trigger: TriggerV0::Timestamp((curr_epoch * EPOCH_LENGTH).try_into().unwrap()),
+          transaction: TransactionSourceV0::CompiledV0(compiled_tx.clone()),
+          crank_reward: None,
+          free_tasks: 0,
+        },
+        TaskReturnV0 {
+          // 1 hour before the next epoch
+          trigger: TriggerV0::Timestamp(
+            ((curr_epoch + 1) * EPOCH_LENGTH - 60 * 60)
+              .try_into()
+              .unwrap(),
+          ),
+          transaction: TransactionSourceV0::CompiledV0(compiled_reschedule_tx.clone()),
+          crank_reward: None,
+          free_tasks: 0,
+        },
+      ]
+      .into_iter(),
     })?
     .used_accounts;
     Ok(RunTaskReturnV0 {
@@ -195,7 +240,7 @@ pub mod hpl_crons {
 }
 
 #[derive(Accounts)]
-pub struct QueueDailyTasks<'info> {
+pub struct QueueEndEpoch<'info> {
   /// CHECK: This account needs to be funded to pay for the cron PDA
   #[account(
         mut,
@@ -203,11 +248,16 @@ pub struct QueueDailyTasks<'info> {
         seeds::program = tuktuk_program::ID,
         bump,
   )]
-  pub payer: AccountInfo<'info>,
+  pub payer: Signer<'info>,
   pub dao: Account<'info, DaoV0>,
   pub iot_sub_dao: Account<'info, SubDaoV0>,
   pub mobile_sub_dao: Account<'info, SubDaoV0>,
-  pub cron_pda: AccountInfo<'info>,
+  #[account(
+    mut,
+    seeds = [b"task_return_account"],
+    bump,
+  )]
+  pub task_return_account: AccountInfo<'info>,
   pub task_queue: Account<'info, TaskQueueV0>,
   pub system_program: Program<'info, System>,
 }
