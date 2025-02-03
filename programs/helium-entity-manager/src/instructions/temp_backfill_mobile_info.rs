ad08dacbd40b4b8ca1baa8be5dd3c36f812913e9
Bryan <bryanzettler@gmail.com>
2025-02-03 15:04:52 -0600

    Develop (#812)
    * add temp endpoint and backfill script for mobile deployment infos (#774)
    
    * add temp endpoint and backfill script for mobile deployment infos
    
    * check for data
    
    * remove log
    
    * Remove unneeded location logic
    
    * running off local ssh db
    
    * update idl
    
    * Update logic branches for updating
    
    * pr feedback
    
    * Bump cargo
    
    * update idl

---
+++ programs/helium-entity-manager/src/instructions/temp_backfill_mobile_info.rs
@@ -0,0 +1,44 @@
+use crate::state::*;
+use anchor_lang::prelude::*;
+use shared_utils::*;
+use std::str::FromStr;
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Default)]
+pub struct TempBackfillMobileInfoArgs {
+  pub location: Option<u64>,
+  pub deployment_info: Option<MobileDeploymentInfoV0>,
+}
+
+#[derive(Accounts)]
+#[instruction(args: TempBackfillMobileInfoArgs)]
+pub struct TempBackfillMobileInfo<'info> {
+  #[account(
+    mut,
+    address = Pubkey::from_str("hprdnjkbziK8NqhThmAn5Gu4XqrBbctX8du4PfJdgvW").unwrap()
+  )]
+  pub payer: Signer<'info>,
+  #[account(mut)]
+  pub mobile_info: Box<Account<'info, MobileHotspotInfoV0>>,
+  pub system_program: Program<'info, System>,
+}
+
+pub fn handler<'info>(
+  ctx: Context<'_, '_, '_, 'info, TempBackfillMobileInfo<'info>>,
+  args: TempBackfillMobileInfoArgs,
+) -> Result<()> {
+  if let Some(new_location) = args.location {
+    ctx.accounts.mobile_info.location = Some(new_location);
+  }
+
+  if let Some(deployment_info) = args.deployment_info {
+    ctx.accounts.mobile_info.deployment_info = Some(deployment_info);
+  }
+
+  resize_to_fit(
+    &ctx.accounts.payer,
+    &ctx.accounts.system_program.to_account_info(),
+    &ctx.accounts.mobile_info,
+  )?;
+
+  Ok(())
+}
