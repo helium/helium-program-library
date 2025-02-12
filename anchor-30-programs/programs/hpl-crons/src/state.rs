006e23740178fce5a9df517a692bd471d0415b1b
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2025-02-12 10:39:58 -0800

    Add position tuktuk claims (#815)

---
+++ anchor-30-programs/programs/hpl-crons/src/state.rs
@@ -0,0 +1,21 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(Default, InitSpace)]
+pub struct EpochTrackerV0 {
+  pub authority: Pubkey,
+  pub dao: Pubkey,
+  pub epoch: u64,
+  pub bump_seed: u8,
+}
+
+#[account]
+#[derive(Default, InitSpace)]
+pub struct DelegationClaimBotV0 {
+  pub delegated_position: Pubkey,
+  pub task_queue: Pubkey,
+  pub rent_refund: Pubkey,
+  pub bump_seed: u8,
+  pub last_claimed_epoch: u64,
+  pub queued: bool,
+}
