9ccb30cf4a0e65ffdf4b850e3cf0449c4ee3058b
Noah Prince <chewinglass123@gmail.com>
2025-02-12 10:00:37 -0800

    Add hotspot claims to hpl-crons

---
+++ anchor-30-programs/programs/hpl-crons/src/external_programs.rs
@@ -0,0 +1,33 @@
+use anchor_lang::prelude::*;
+
+pub mod helium_entity_manager {
+  use anchor_lang::{declare_id, declare_program};
+
+  declare_id!("hemjuPXBpNvggtaUnN1MwT3wrdhttKEfosTcc2P9Pg8");
+
+  declare_program!(helium_entity_manager);
+}
+
+pub mod no_emit {
+  use anchor_lang::{declare_id, declare_program};
+
+  declare_id!("noEmmgLmQdk6DLiPV8CSwQv3qQDyGEhz9m5A4zhtByv");
+
+  declare_program!(no_emit);
+}
+
+pub mod helium_sub_daos {
+  use anchor_lang::{declare_id, declare_program};
+
+  declare_id!("hdaoVTCqhfHHo75XdAMxBKdUqvq1i5bF23sisBqVgGR");
+
+  declare_program!(helium_sub_daos);
+}
+
+pub mod voter_stake_registry {
+  use anchor_lang::{declare_id, declare_program};
+
+  declare_id!("hvsrNC3NKbcryqDs2DocYHZ9yPKEVzdSjQG6RVtK1s8");
+
+  declare_program!(voter_stake_registry);
+}
