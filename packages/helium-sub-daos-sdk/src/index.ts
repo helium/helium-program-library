50402c7b4c21e398d83286fac0ded663b5caa92f
Bryan <bryanzettler@gmail.com>
2025-03-13 10:28:10 -0500

    correct season logic (#873)
    * correct season logic
    
    * fix
    
    * create utils for helium-sub-daos and export getLockupEffectiveEndTs

--- packages/helium-sub-daos-sdk/src/index.ts
+++ packages/helium-sub-daos-sdk/src/index.ts
@@ -1,12 +1,5 @@
-import { BN } from "bn.js";
-
 export * from "./init";
-
-export function delegatorRewardsPercent(percent: number) {
-  return new BN(Math.floor(percent * Math.pow(10, 8)));
-}
-
 export * from "./constants";
 export * from "./pdas";
 export * from "./resolvers";
-
+export * from "./utils";
---
+++ packages/helium-sub-daos-sdk/src/utils.ts
@@ -0,0 +1,19 @@
+import BN from "bn.js";
+import { EPOCH_LENGTH } from "./constants";
+
+export const delegatorRewardsPercent = (percent: number) => {
+  return new BN(Math.floor(percent * Math.pow(10, 8)));
+};
+
+export const currentEpoch = (unixTime: BN): BN => {
+  return new BN(Math.floor(unixTime.toNumber() / EPOCH_LENGTH));
+};
+
+export const getLockupEffectiveEndTs = (lockup: {
+  kind: any;
+  endTs: BN;
+}): BN => {
+  const isConstant = (Object.keys(lockup.kind)[0] as string) === "constant";
+  // max i64 for constant lockups
+  return isConstant ? new BN("9223372036854775807") : lockup.endTs;
+};
