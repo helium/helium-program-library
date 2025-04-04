4c49dc3d88f32187715917ebd15a3939eb915680
Bryan <bryanzettler@gmail.com>
2025-04-04 12:13:19 -0500

    Feat/geocoder service (#908)
    * Add geocoder-service
    
    * add gecoder to docker tests
    
    * expose 8000
    
    * remove comment
    
    * update readme
    
    * user agent
    
    * update entrypoint.sh
    
    * tweak
    
    * fix data dir
    
    * remove redundant code

---
+++ packages/geocoder-service/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+DATA_DIR="/usr/src/app/photon_data"
+ES_INDEX_DIR="${DATA_DIR}/elasticsearch"
+USER_AGENT="docker: helium/geocoder-service"
+DOWNLOAD_URL="http://download1.graphhopper.com/public/photon-db-latest.tar.bz2"
+
+# Download elasticsearch index
+if [ ! -d "${ES_INDEX_DIR}" ]; then
+    echo "Downloading search index..."
+    if ! wget -q --show-progress --user-agent="${USER_AGENT}" -O - "${DOWNLOAD_URL}" | pbzip2 -cd | tar x; then
+        echo "Error: Failed to download or extract the search index."
+        exit 1
+    fi
+fi
+
+# Start service if elastic index exists
+if [ -d "${ES_INDEX_DIR}" ]; then
+    echo "Starting service..."
+    exec java -jar photon.jar "$@"
+else
+    echo "Could not start service, the search index could not be found"
+    exit 1
+fi
