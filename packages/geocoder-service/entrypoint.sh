4933e740f9bb2ab1a8288b9ed2120c9c8042a6d3
bry <bryanzettler@gmail.com>
2025-04-04 14:03:44 -0500

    use /data for geocoder service

--- packages/geocoder-service/entrypoint.sh
+++ packages/geocoder-service/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DATA_DIR="/usr/src/app/photon_data"
+DATA_DIR="/data"
 ES_INDEX_DIR="${DATA_DIR}/elasticsearch"
 USER_AGENT="docker: helium/geocoder-service"
 DOWNLOAD_URL="http://download1.graphhopper.com/public/photon-db-latest.tar.bz2"
@@ -9,7 +9,7 @@ DOWNLOAD_URL="http://download1.graphhopper.com/public/photon-db-latest.tar.bz2"
 # Download elasticsearch index
 if [ ! -d "${ES_INDEX_DIR}" ]; then
     echo "Downloading search index..."
-    if ! wget -q --show-progress --user-agent="${USER_AGENT}" -O - "${DOWNLOAD_URL}" | pbzip2 -cd | tar x; then
+    if ! wget -q --show-progress --user-agent="${USER_AGENT}" -O - "${DOWNLOAD_URL}" | pbzip2 -cd | tar x -C "${DATA_DIR}"; then
         echo "Error: Failed to download or extract the search index."
         exit 1
     fi
@@ -18,7 +18,7 @@ fi
 # Start service if elastic index exists
 if [ -d "${ES_INDEX_DIR}" ]; then
     echo "Starting service..."
-    exec java -jar photon.jar "$@"
+    exec java -jar photon.jar --data-dir="${DATA_DIR}" "$@"
 else
     echo "Could not start service, the search index could not be found"
     exit 1
