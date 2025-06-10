2fad4b800a2a9a0fe42d0e6e576bf15470b0eef2
Noah Prince <chewinglass123@gmail.com>
2025-06-10 07:56:12 -0700

    Implement delegateAll

--- packages/automation-hooks/src/hooks/index.ts
+++ packages/automation-hooks/src/hooks/index.ts
@@ -2,4 +2,4 @@
 export { useAutomateHotspotClaims } from "./useAutomateHotspotClaims";
 export { useCronJob } from "./useCronJob";
 export { useTaskQueue } from "./useTaskQueue";
-export { useDelegationClaimBot } from "./useDelegationClaimBot";
+export { useDelegationClaimBot, useDelegationClaimBots } from "./useDelegationClaimBot";
