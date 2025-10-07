f439758243fef9a704f6a13c2c2eacdec176f8c6
Noah Prince <chewinglass123@gmail.com>
2025-10-07 16:20:01 -0700

    Add squads v4 support to cli

--- packages/helium-admin-cli/src/utils.ts
+++ packages/helium-admin-cli/src/utils.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { idlAddress } from "@coral-xyz/anchor/dist/cjs/idl";
 import {
+  bulkSendTransactions,
   createAtaAndMintInstructions,
   createMintInstructions,
   sendInstructions,
@@ -57,9 +58,11 @@ import {
   VersionedTransaction,
 } from "@solana/web3.js";
 import Squads, { getTxPDA } from "@sqds/sdk";
+import * as multisig from "@sqds/multisig";
 import { BN } from "bn.js";
 import fs from "fs";
 import fetch from "node-fetch";
+import { packageInstructions } from "./squads-batch-optimizer";
 
 const SECONDS_PER_DAY = 86400;
 
@@ -677,6 +680,122 @@ export async function createCloseBufferInstruction(
     programId: bpfUpgradableLoaderId,
     data: Buffer.from([5, 0, 0, 0]), // Upgrade instruction bincode
   });
+}
+
+export async function sendInstructionsOrSquadsV4({
+  provider,
+  instructions,
+  signers = [],
+  payer = provider.wallet.publicKey,
+  commitment = "confirmed",
+  idlErrors = new Map(),
+  multisig: multisigPda,
+}: {
+  provider: anchor.AnchorProvider;
+  instructions: TransactionInstruction[];
+  signers?: Signer[];
+  payer?: PublicKey;
+  commitment?: Commitment;
+  idlErrors?: Map<number, string>;
+  multisig?: PublicKey;
+}): Promise<string | undefined> {
+  if (!multisig) {
+    return await sendInstructions(
+      provider,
+      await withPriorityFees({
+        connection: provider.connection,
+        computeUnits: 1000000,
+        instructions,
+      }),
+      signers,
+      payer,
+      commitment,
+      idlErrors
+    );
+  }
+
+  const signerSet = new Set(
+    instructions
+      .map((ix) =>
+        ix.keys.filter((k) => k.isSigner).map((k) => k.pubkey.toBase58())
+      )
+      .flat()
+  );
+  const signerKeys = Array.from(signerSet).map((k) => new PublicKey(k));
+
+  const nonMissingSignerIxs = instructions.filter(
+    (ix) =>
+      !ix.keys.some(
+        (k) => k.isSigner && !k.pubkey.equals(provider.wallet.publicKey)
+      )
+  );
+  const squadsSignatures = signerKeys.filter(
+    (k) =>
+      !k.equals(provider.wallet.publicKey) &&
+      !signers.some((s) => s.publicKey.equals(k))
+  );
+
+  if (squadsSignatures.length == 0) {
+    return await sendInstructions(
+      provider,
+      await withPriorityFees({
+        connection: provider.connection,
+        computeUnits: 1000000,
+        instructions: nonMissingSignerIxs,
+      }),
+      signers,
+      payer,
+      commitment,
+      idlErrors
+    );
+  }
+
+  if (squadsSignatures.length >= 2) {
+    throw new Error("Too many missing signatures");
+  }
+
+  const multisigInfo = await multisig.accounts.Multisig.fromAccountAddress(
+    provider.connection,
+    multisigPda!
+  );
+
+  const transactionIndex = Number(multisigInfo.transactionIndex) + 1;
+  const createBatchIx = await multisig.instructions.batchCreate({
+    batchIndex: BigInt(transactionIndex),
+    creator: provider.wallet.publicKey,
+    multisigPda: multisigPda!,
+    vaultIndex: 0,
+    memo: "Helium-admin-cli batch",
+  });
+  await sendInstructions(provider, await withPriorityFees({
+    connection: provider.connection,
+    instructions: [createBatchIx],
+  }));
+
+  const { transactionMessages, failedBuckets } = await packageInstructions(instructions.map(ix => [ix]), [])
+  if (failedBuckets.length > 0) {
+    throw new Error(`Failed to package instructions: ${failedBuckets.join(", ")}`);
+  }
+
+  await bulkSendTransactions(provider, await Promise.all(transactionMessages.map(ms =>
+  ({
+    feePayer: provider.wallet.publicKey,
+    instructions: ms.instructions,
+  })
+  )))
+
+  await sendInstructions(
+    provider,
+    await withPriorityFees({
+      connection: provider.connection,
+      instructions: [await multisig.instructions.proposalCreate({
+        multisigPda: multisigPda!,
+        transactionIndex: BigInt(transactionIndex),
+        creator: provider.wallet.publicKey,
+      })]
+    }),
+    signers
+  )
 }
 
 export async function sendInstructionsOrSquads({
