5a73f2f69d3a43a83aae43dbba687ff3987c3ddf
bry <bryanzettler@gmail.com>
2025-12-19 12:30:46 -0600

    fix sql

--- utils/atomic-data-publisher/src/sql/construct_entity_ownership_changes.sql
+++ utils/atomic-data-publisher/src/sql/construct_entity_ownership_changes.sql
@@ -11,42 +11,20 @@ WITH asset_owner_changes AS (
     ao.asset,
     ao.last_block as block,
     encode(kta.entity_key, 'hex') as pub_key,
-    CASE WHEN wp_check.address IS NOT NULL THEN wp_check.owner ELSE ao.owner END as owner,
-    CASE WHEN wp_check.address IS NOT NULL THEN 'welcome_pack_owner' ELSE 'direct_owner' END as owner_type,
+    CASE WHEN wp.address IS NOT NULL THEN wp.owner ELSE ao.owner END as owner,
+    CASE WHEN wp.address IS NOT NULL THEN 'welcome_pack_owner' ELSE 'direct_owner' END as owner_type,
     'entity_ownership' as change_type
   FROM asset_owners ao
   INNER JOIN key_to_assets kta ON kta.asset = ao.asset
   LEFT JOIN iot_hotspot_infos ihi ON ihi.asset = ao.asset
   LEFT JOIN mobile_hotspot_infos mhi ON mhi.asset = ao.asset
-  LEFT JOIN welcome_packs wp_check ON wp_check.address = ao.owner
+  LEFT JOIN welcome_packs wp ON wp.address = ao.owner
   WHERE (ihi.asset IS NOT NULL OR mhi.asset IS NOT NULL)
     AND kta.entity_key IS NOT NULL
     AND ao.asset IS NOT NULL
     AND ao.owner IS NOT NULL
     AND ao.last_block > $1
     AND ao.last_block <= $2
-    -- Exclude if welcome pack was also created in this block range (let welcome_pack_changes handle it)
-    AND (wp_check.address IS NULL OR wp_check.last_block <= $1 OR wp_check.last_block > $2)
-),
-welcome_pack_changes AS (
-  -- Get welcome pack owner changes in the block range
-  SELECT
-    wp.asset,
-    wp.last_block as block,
-    encode(kta.entity_key, 'hex') as pub_key,
-    wp.owner,
-    'welcome_pack_owner' as owner_type,
-    'entity_ownership' as change_type
-  FROM welcome_packs wp
-  INNER JOIN key_to_assets kta ON kta.asset = wp.asset
-  LEFT JOIN iot_hotspot_infos ihi ON ihi.asset = wp.asset
-  LEFT JOIN mobile_hotspot_infos mhi ON mhi.asset = wp.asset
-  WHERE (ihi.asset IS NOT NULL OR mhi.asset IS NOT NULL)
-    AND kta.entity_key IS NOT NULL
-    AND wp.asset IS NOT NULL
-    AND wp.owner IS NOT NULL
-    AND wp.last_block > $1
-    AND wp.last_block <= $2
 ),
 hotspots_onboarded_in_range AS (
   -- Get hotspots onboarded in this block range
@@ -75,9 +53,6 @@ newly_onboarded_hotspots AS (
 ownership_changes AS (
   SELECT asset, block, pub_key, owner, owner_type, change_type
   FROM asset_owner_changes
-  UNION ALL
-  SELECT asset, block, pub_key, owner, owner_type, change_type
-  FROM welcome_pack_changes
   UNION ALL
   SELECT asset, block, pub_key, owner, owner_type, change_type
   FROM newly_onboarded_hotspots
