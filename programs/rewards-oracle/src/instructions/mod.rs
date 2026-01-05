acf08ae88dd42300eeb7d07c17dc0bdce409d7bf
bry <bryanzettler@gmail.com>
2026-01-05 13:50:16 -0600

    drop temp endpoints and scripts

--- programs/rewards-oracle/src/instructions/mod.rs
+++ programs/rewards-oracle/src/instructions/mod.rs
@@ -1,9 +1,7 @@
 pub mod set_current_rewards_wrapper_v0;
 pub mod set_current_rewards_wrapper_v1;
 pub mod set_current_rewards_wrapper_v2;
-pub mod temp_close_recipient_wrapper_v0;
 
 pub use set_current_rewards_wrapper_v0::*;
 pub use set_current_rewards_wrapper_v1::*;
 pub use set_current_rewards_wrapper_v2::*;
-pub use temp_close_recipient_wrapper_v0::*;
