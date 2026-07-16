fd9e25e32ceec2b491383f791133bb328c11d693
bry <bryanzettler@gmail.com>
2026-07-16 10:56:39 -0500

    fix(ci): pin rust toolchain to 1.85.0 and allow uint macro lint

--- utils/shared-utils/src/uint.rs
+++ utils/shared-utils/src/uint.rs
@@ -5,6 +5,9 @@
 #![allow(clippy::ptr_offset_with_cast)]
 #![allow(clippy::manual_range_contains)]
 #![allow(clippy::manual_div_ceil)]
+// construct_uint! in uint 0.9.5 emits trailing semicolons in expression
+// position, denied under future_incompatible on newer toolchains
+#![allow(semicolon_in_expressions_from_macros)]
 
 use uint::construct_uint;
 
