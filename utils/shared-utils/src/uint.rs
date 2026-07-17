7a7516dfd6e400f8323aab2d4830f9ec6ef57a69
bry <bryanzettler@gmail.com>
2026-07-17 15:33:25 -0500

    fix tests

--- utils/shared-utils/src/uint.rs
+++ utils/shared-utils/src/uint.rs
@@ -5,6 +5,9 @@
 #![allow(clippy::ptr_offset_with_cast)]
 #![allow(clippy::manual_range_contains)]
 #![allow(clippy::manual_div_ceil)]
+// uint's construct_uint! expands macros with trailing semicolons in expression
+// position; newer rustc denies this by default (rust-lang/rust#79813).
+#![allow(semicolon_in_expressions_from_macros)]
 
 use uint::construct_uint;
 
